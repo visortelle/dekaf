a7a2fcf8307ddab5b1b56702939171cc2567b095
visortelle <visortelle@gmail.com>
2022-05-28 17:19:29 +0200

    Initial commit

---
+++ pulsar-ui/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Pulsar UI</title>
+        <meta name="description" content="Pulsar UI" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default Home
