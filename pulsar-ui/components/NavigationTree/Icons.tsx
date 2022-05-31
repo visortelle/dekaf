ba4bad7563ad85b9187d216989fd471dd957adb4
visortelle <visortelle@gmail.com>
2022-05-31 10:50:13 +0200

    Force reload tree data on node type visibility change

---
+++ pulsar-ui/components/NavigationTree/Icons.tsx
@@ -0,0 +1,75 @@
+import s from './Icons.module.css';
+
+export type NodeIconsProps = {
+  title: string;
+  textColor: string;
+  backgroundColor: string;
+  isExpandable: boolean;
+  isExpanded: boolean;
+  onClick: () => void
+  className?: string
+}
+export const NodeIcon: React.FC<NodeIconsProps> = (props) => {
+  return (
+    <div
+      style={{ color: props.textColor, backgroundColor: props.backgroundColor }}
+      className={`${s.NodeIcon} ${props.isExpanded ? s.NodeIconExpanded : ''} ${props.isExpandable ? s.NodeIconExpandable : ''} ${props.className || ''}`}
+      onClick={props.onClick}
+    >
+      {props.title}
+    </div>
+  );
+}
+
+export type TopicIconProps = {
+  onClick: () => void;
+  isExpanded: boolean;
+  isExpandable: boolean;
+  className?: string;
+}
+export const TopicIcon: React.FC<TopicIconProps> = (props) => {
+  return <NodeIcon
+    title="to"
+    textColor='#fff'
+    backgroundColor='#00ab28'
+    onClick={props.onClick}
+    isExpanded={props.isExpanded}
+    isExpandable={props.isExpandable}
+    className={props.className}
+  />
+}
+
+export type NamespaceIconProps = {
+  onClick: () => void;
+  isExpanded: boolean;
+  className?: string;
+}
+export const NamespaceIcon: React.FC<NamespaceIconProps> = (props) => {
+  return <NodeIcon
+    title="ns"
+    textColor='#fff'
+    backgroundColor='#fe6e6e'
+    onClick={props.onClick}
+    isExpanded={props.isExpanded}
+    isExpandable={true}
+    className={props.className}
+  />
+
+}
+
+export type TenantIconProps = {
+  onClick: () => void;
+  isExpanded: boolean;
+  className?: string;
+}
+export const TenantIcon: React.FC<TenantIconProps> = (props) => {
+  return <NodeIcon
+    title="te"
+    textColor='#fff'
+    backgroundColor='#276ff4'
+    onClick={props.onClick}
+    isExpanded={props.isExpanded}
+    isExpandable={true}
+    className={props.className}
+  />
+}
