18beae718fe2f62f376317b16442ee6cc151c73c
visortelle <visortelle@gmail.com>
2022-05-31 12:43:40 +0200

    Setup router

---
+++ pulsar-ui/components/NoSsr/NoSsr.tsx
@@ -0,0 +1,10 @@
+import dynamic from 'next/dynamic'
+import React from 'react'
+
+const NoSsr = (props: { children: React.ReactNode }) => (
+  <>{props.children}</>
+)
+
+export default dynamic(() => Promise.resolve(NoSsr), {
+  ssr: false
+})
--- pulsar-ui/pages/index.tsx
+++ pulsar-ui/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import HomePage from '../components/HomePage/HomePage'
+import Router from '../components/Router/Router'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Pulsar UI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomePage />
+      <Router />
     </>
   )
 }
