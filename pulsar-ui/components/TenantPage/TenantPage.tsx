a42124345145434c5e5597536c231b3c5781b4af
visortelle <visortelle@gmail.com>
2022-06-01 18:21:51 +0200

    Add toolbar to tenants page

--- pulsar-ui/components/TenantPage/TenantPage.tsx
+++ pulsar-ui/components/TenantPage/TenantPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BreadCrumbsAtPageTop } from '../BreadCrumbs/BreadCrumbs';
 import s from './TenantPage.module.css'
+import Button from '../ui/Button/Button';
 
 export type TenantPageProps = {
   tenant: string;
@@ -19,6 +20,38 @@ const TenantPage: React.FC<TenantPageProps> = (props) => {
         ]}
       />
 
+      <div className={s.PageContent}>
+        <div className={s.Toolbar}>
+          <div className={s.ToolbarButton}>
+            <Button
+              title='Namespaces'
+              onClick={() => undefined}
+              type={'regular'}
+            />
+          </div>
+          <div className={s.ToolbarButton}>
+            <Button
+              title='Configuration'
+              onClick={() => undefined}
+              type={'regular'}
+            />
+          </div>
+          <div className={s.ToolbarButton}>
+            <Button
+              title='Delete'
+              onClick={() => undefined}
+              type={'danger'}
+            />
+          </div>
+          <div className={s.ToolbarActionButton}>
+            <Button
+              title='Create namespace'
+              onClick={() => undefined}
+              type={'primary'}
+            />
+          </div>
+        </div>
+      </div>
       tenant page
     </div>
   );
--- pulsar-ui/components/ui/Button/Button.tsx
+++ pulsar-ui/components/ui/Button/Button.tsx
@@ -3,11 +3,24 @@ import s from './Button.module.css';
 export type ButtonProps = {
  onClick: () => void,
  title: string
+ type: 'primary' | 'regular' | 'danger',
 }
-const Button: React.FC<ButtonProps> = ({ onClick, title }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, title, type }) => {
+  let className = '';
+  switch(type) {
+    case 'regular': className = s.Regular; break;
+    case 'primary': className = s.Primary; break;
+    case 'danger': className = s.Danger; break;
+  }
+
   return (
-    <div className={s.Button} onClick={onClick}>
+    <div
+      className={`${s.Button} ${className}`}
+      onClick={onClick}
+    >
       {title}
     </div>
   );
 }
+
+export default Button;
