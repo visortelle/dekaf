adc6ceb2fe3406a1c3a5d9eacd664c69f9e80b8b
visortelle <visortelle@gmail.com>
2022-06-01 14:02:52 +0200

    Add breadcrumbs

--- pulsar-ui/components/BreadCrumbs/BreadCrumbs.tsx
+++ pulsar-ui/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
 import s from './BreadCrumbs.module.css'
+import { TenantIcon, NamespaceIcon, TopicIcon } from '../Icons/Icons';
+import SvgIcon from '../ui/SvgIcon/SvgIcon';
+import arrowIcon from '!!raw-loader!./arrow.svg';
+import { Link } from 'react-router-dom';
 
-export type BreadCrumbsProps = {};
+export type CrumbType = 'tenant' | 'namespace' | 'topic';
+export type Crumb = {
+  id: string;
+  type: CrumbType;
+  value: string
+};
+
+export type BreadCrumbsProps = {
+  crumbs: Crumb[];
+};
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = (props) => {
+  const tenant = props.crumbs[0]?.value;
+  const namespace = props.crumbs[1]?.value;
+  const topic = props.crumbs[2]?.value;
+
+  const renderCrumb = (crumb: Crumb, i: number, total: number) => {
+    let icon = null;
+    switch (crumb.type) {
+      case 'tenant': icon = <TenantIcon />; break;
+      case 'namespace': icon = <NamespaceIcon />; break;
+      case 'topic': icon = <TopicIcon />; break;
+    }
+
+    const isLast = i === total - 1;
+
+    let href = '#';
+    switch (crumb.type) {
+      case 'tenant': href = `/tenants/${tenant}`; break;
+      case 'namespace': href = `/tenants/${tenant}/namespaces/${namespace}`; break;
+      case 'topic': href = `/tenants/${tenant}/namespaces/${namespace}/topics/${topic}`; break;
+    }
+
+    return (
+      <Link key={crumb.id} className={s.Crumb} to={href}>
+        <div className={s.CrumbIcon}>{icon}</div>
+        <div className={s.CrumbTitle}>{crumb.value}</div>
+        {!isLast && <div className={s.CrumbArrow}><SvgIcon svg={arrowIcon} /></div>}
+      </Link>
+    );
+  }
+
   return (
     <div className={s.BreadCrumbs}>
-
+      {props.crumbs.map((crumb, i) => renderCrumb(crumb, i, props.crumbs.length))}
     </div>
   );
 }
 
 export default BreadCrumbs;
+
+
+export const BreadCrumbsAtPageTop: React.FC<BreadCrumbsProps> = (props) => {
+  return (
+    <div className={s.BreadCrumbsAtPageTop}>
+      <BreadCrumbs {...props} />
+    </div>
+  );
+}
--- pulsar-ui/components/NamespacePage/NamespacePage.tsx
+++ pulsar-ui/components/NamespacePage/NamespacePage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { BreadCrumbsAtPageTop } from '../BreadCrumbs/BreadCrumbs';
 import s from './NamespacePage.module.css'
 
-export type NamespacePageProps = {};
+export type NamespacePageProps = {
+  tenant: string;
+  namespace: string;
+};
 
 const NamespacePage: React.FC<NamespacePageProps> = (props) => {
   return (
-    <div className={s.TenantPage}>namespace page</div>
+    <div className={s.Page}>
+      <BreadCrumbsAtPageTop
+        crumbs={[
+          {
+            id: `tenant-${props.tenant}`,
+            value: props.tenant,
+            type: 'tenant',
+          },
+          {
+            id: `namespace-${props.namespace}`,
+            value: props.namespace,
+            type: 'namespace',
+          }
+        ]}
+      />
+      namespace page
+    </div>
   );
 }
 
