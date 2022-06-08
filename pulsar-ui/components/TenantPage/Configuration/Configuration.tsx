a0376ae986e9799654f51de5fb574f113268c44d
visortelle <visortelle@gmail.com>
2022-06-08 18:18:50 +0200

    Configure allowed clusters

--- pulsar-ui/components/TenantPage/Configuration/Configuration.tsx
+++ pulsar-ui/components/TenantPage/Configuration/Configuration.tsx
@@ -7,6 +7,8 @@ import * as Notifications from '../../contexts/Notifications';
 import * as PulsarAdminClient from '../../contexts/PulsarAdminClient';
 import * as Either from 'fp-ts/lib/Either';
 import StringEditor from '../../ConfigurationTable/String/StringEditor/StringEditor';
+import SelectInput, { undefinedListItem } from '../../ConfigurationTable/SelectInput/SelectInput';
+import { time } from 'console';
 
 export type ConfigurationProps = {
   tenant: string
@@ -22,6 +24,15 @@ const Configuration: React.FC<ConfigurationProps> = (props) => {
 
   const onTenantUpdateError = (err: string) => notifyError(`Can't update tenant configuration: ${err}`);
 
+  const { data: clusters, error: clustersError } = useSWR(
+    ['pulsar', 'clusters'],
+    async () => await adminClient.clusters.getClusters()
+  );
+
+  if (clustersError) {
+    notifyError(`Unable to get clusters list: ${clustersError}`)
+  }
+
   const { data: configuration, error: configurationError } = useSWR(
     ['pulsar', 'tenants', props.tenant, 'configuration'],
     async () => await adminClient.tenants.getTenantAdmin(props.tenant) as unknown as { adminRoles: string[], allowedClusters: string[] }
@@ -35,9 +46,9 @@ const Configuration: React.FC<ConfigurationProps> = (props) => {
     type: 'list',
     value: configuration?.adminRoles || [],
     getId: (v) => v,
-    render: (v) => <div>{v}</div>,
+    render: (v) => <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{v}</span>,
     editor: {
-      render: (v, onChange) => <StringEditor value={v} onChange={onChange} placeholder="Add role" />,
+      render: (v, onChange) => <StringEditor value={v} onChange={onChange} placeholder="Type role" />,
       initialValue: '',
     },
     onRemove: (id) => {
@@ -70,7 +81,14 @@ const Configuration: React.FC<ConfigurationProps> = (props) => {
     getId: (v) => v,
     render: (v) => <div>{v}</div>,
     editor: {
-      render: (v, onChange) => <StringEditor value={v} onChange={onChange} placeholder="Add cluster" />,
+      render: (v, onChange) => (
+        <SelectInput
+          list={(clusters || []).filter(c => !allowedClusters.value.some(ac => ac === c)).map(c => ({ id: c, title: c }))}
+          value={v}
+          prependWithEmptyItem={true}
+          onChange={onChange}
+          placeholder="Select cluster"
+        />),
       initialValue: '',
     },
     onRemove: configuration?.allowedClusters.length === 1 ? undefined : (id) => {
@@ -100,14 +118,14 @@ const Configuration: React.FC<ConfigurationProps> = (props) => {
   const adminRolesField: ConfigurationField<AdminRolesValue> = {
     id: "adminRoles",
     title: "Admin roles",
-    description: "Roles that can manage the tenant.",
+    description: "List of authenticated roles allowed to manage this tenant.",
     value: { ...adminRoles, value: adminRoles.value.sort((a, b) => a.localeCompare(b, 'en', { ignorePunctuation: true })) },
   }
 
   const allowedClustersField: ConfigurationField<AllowedClustersValue> = {
     id: "allowedClusters",
     title: "Allowed clusters",
-    description: "Roles that can manage the tenant",
+    description: "List of clusters that this tenant is restricted on.",
     value: allowedClusters,
   }
 
