3123216762ca9b26fd887cb813a63b3dd28d4861
visortelle <visortelle@gmail.com>
2022-06-12 11:59:13 +0200

    Implement persistence field

---
+++ pulsar-ui/components/NamespacePage/Policies/fields/persistence.tsx
@@ -0,0 +1,167 @@
+import * as Notifications from '../../../contexts/Notifications';
+import * as PulsarAdminClient from '../../../contexts/PulsarAdminClient';
+import useSWR, { useSWRConfig } from "swr";
+import { ConfigurationField } from "../../../ConfigurationTable/ConfigurationTable";
+import s from './persistence.module.css';
+import sf from '../../../ConfigurationTable/form.module.css';
+import Input from "../../../ConfigurationTable/Input/Input";
+import { useEffect, useState } from 'react';
+import Button from '../../../ui/Button/Button';
+import * as Either from 'fp-ts/Either';
+
+export type Persistence = {
+  bookkeeperAckQuorum: number;
+  bookkeeperEnsemble: number;
+  bookkeeperWriteQuorum: number;
+  mlMarkDeleteMaxRate: number;
+}
+
+export type PersistenceInputProps = {
+  value: Persistence;
+  onChange: (value: Persistence) => void;
+};
+export const PersistenceInput: React.FC<PersistenceInputProps> = (props) => {
+  const [persistence, setPersistence] = useState<Persistence>(props.value);
+  const [validationResult, setValidationResult] = useState<Either.Either<React.ReactElement, void>>(Either.right(undefined));
+
+  const validatePersistence = (p: Persistence): void => {
+    // Source: https://github.com/apache/pulsar/blob/6734a3bf77793419898dd1c8421da039dc117f6e/pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/AdminResource.java#L812
+    const isValid = (
+      (p.bookkeeperEnsemble >= p.bookkeeperWriteQuorum) &&
+      (p.bookkeeperWriteQuorum >= p.bookkeeperAckQuorum)
+    );
+
+    if (!isValid) {
+      setValidationResult(() =>
+        Either.left(
+          <div style={{ color: `var(--accent-color-red)`, marginBottom: '12rem' }}>
+            <span><strong>Ensemble</strong> must be &gt;= <strong>Write quorum</strong>.</span>
+            <br />
+            <span><strong>Write quorum</strong> must be &gt;= <strong>Ack quorum</strong>.</span>
+          </div>
+        )
+      );
+      return;
+    }
+
+    setValidationResult(() => Either.right(undefined));
+  }
+
+  useEffect(() => {
+    setPersistence(() => props.value);
+  }, [props.value]);
+
+  useEffect(() => {
+    validatePersistence(persistence);
+  }, [persistence]);
+
+  return (
+    <div className={s.PersistenceInput}>
+      <div className={sf.FormItem}>
+        <strong className={sf.FormLabel}>Ack quorum</strong>
+        <Input
+          type='number'
+          onChange={(v) => setPersistence({ ...persistence, bookkeeperAckQuorum: Number(v) })}
+          value={String(persistence.bookkeeperAckQuorum)}
+        />
+      </div>
+
+      <div className={sf.FormItem}>
+        <strong className={sf.FormLabel}>Ensemble</strong>
+        <Input
+          type='number'
+          onChange={(v) => setPersistence({ ...persistence, bookkeeperEnsemble: Number(v) })}
+          value={String(persistence.bookkeeperEnsemble)}
+        />
+      </div>
+
+      <div className={sf.FormItem}>
+        <strong className={sf.FormLabel}>Write quorum</strong>
+        <Input
+          type='number'
+          onChange={(v) => setPersistence({ ...persistence, bookkeeperWriteQuorum: Number(v) })}
+          value={String(persistence.bookkeeperWriteQuorum)}
+        />
+      </div>
+
+      <div className={sf.FormItem}>
+        <strong className={sf.FormLabel}>Mark delete max rate</strong>
+        <Input
+          type='number'
+          onChange={(v) => setPersistence({ ...persistence, mlMarkDeleteMaxRate: Number(v) })}
+          value={String(persistence.mlMarkDeleteMaxRate)}
+        />
+      </div>
+
+      {Either.isLeft(validationResult) && (
+        <div>
+          {validationResult.left}
+        </div>
+      )}
+
+      <div className={`${sf.FormItem} ${s.UpdateButton}`}>
+        <Button
+          onClick={() => props.onChange(persistence)}
+          title="Update"
+          type="primary"
+          disabled={Either.isLeft(validationResult)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export type FieldInputProps = {
+  tenant: string;
+  namespace: string;
+}
+
+export const FieldInput: React.FC<FieldInputProps> = (props) => {
+  const adminClient = PulsarAdminClient.useContext().client;
+  const { notifyError } = Notifications.useContext();
+  const { mutate } = useSWRConfig();
+
+  const onUpdateError = (err: string) => notifyError(`Can't update persistency policies. ${err}`);
+  const swrKey = ['pulsar', 'tenants', props.tenant, 'namespaces', props.namespace, 'policies', 'persistence'];
+
+  const { data: persistence, error: persistenceError } = useSWR(
+    swrKey,
+    async () => await adminClient.namespaces.getPersistence(props.tenant, props.namespace)
+  );
+
+  if (persistenceError) {
+    notifyError(`Unable to get replication clusters list: ${persistenceError}`);
+  }
+
+  return (
+    <PersistenceInput
+      value={{
+        bookkeeperAckQuorum: persistence?.bookkeeperAckQuorum || 0,
+        bookkeeperEnsemble: persistence?.bookkeeperEnsemble || 0,
+        bookkeeperWriteQuorum: persistence?.bookkeeperWriteQuorum || 0,
+        mlMarkDeleteMaxRate: persistence?.managedLedgerMaxMarkDeleteRate || 0,
+      }}
+      onChange={async (value) => {
+        await adminClient.namespaces.setPersistence(
+          props.tenant,
+          props.namespace,
+          {
+            bookkeeperEnsemble: value.bookkeeperEnsemble,
+            bookkeeperWriteQuorum: value.bookkeeperWriteQuorum,
+            bookkeeperAckQuorum: value.bookkeeperAckQuorum,
+            managedLedgerMaxMarkDeleteRate: value.mlMarkDeleteMaxRate
+          }).catch(onUpdateError);
+        await mutate(swrKey);
+      }}
+    />
+  )
+}
+
+const field = (props: FieldInputProps): ConfigurationField => ({
+  id: 'persistence',
+  title: 'Persistence',
+  description: <span>List of clusters that will be used for replication.</span>,
+  input: <FieldInput {...props} />
+});
+
+export default field;
