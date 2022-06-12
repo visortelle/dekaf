5f854c74295f9b473abc2853a4efbd51b5720c0b
visortelle <visortelle@gmail.com>
2022-06-12 18:15:04 +0200

    Implement auto subscription creation field

---
+++ pulsar-ui/components/NamespacePage/Policies/fields/auto-subscription-creation.tsx
@@ -0,0 +1,57 @@
+import SelectInput from "../../../ConfigurationTable/SelectInput/SelectInputWithUpdateConfirmation";
+import * as Notifications from '../../../contexts/Notifications';
+import * as PulsarAdminClient from '../../../contexts/PulsarAdminClient';
+import useSWR, { useSWRConfig } from "swr";
+import { ConfigurationField } from "../../../ConfigurationTable/ConfigurationTable";
+
+const policyId = 'autoSubscriptionCreation';
+
+export type FieldInputProps = {
+  tenant: string;
+  namespace: string;
+}
+
+type AutoSubscriptionCreation = 'enabled' | 'disabled';
+
+export const FieldInput: React.FC<FieldInputProps> = (props) => {
+  const adminClient = PulsarAdminClient.useContext().client;
+  const { notifyError } = Notifications.useContext();
+  const { mutate } = useSWRConfig();
+
+  const onUpdateError = (err: string) => notifyError(`Can't update auto subscription creation. ${err}`);
+  const swrKey = ['pulsar', 'tenants', props.tenant, 'namespaces', props.namespace, 'policies', policyId];
+
+  const { data: autoSubscriptionCreation, error: autoSubscriptionCreationError } = useSWR(
+    swrKey,
+    async () => Boolean(await adminClient.namespaces.getAutoSubscriptionCreation(props.tenant, props.namespace)) // XXX - see https://github.com/apache/pulsar/issues/16024
+  );
+
+  if (autoSubscriptionCreationError) {
+    notifyError(`Unable to get deduplication: ${autoSubscriptionCreationError}`);
+  }
+
+  return (
+    <SelectInput<AutoSubscriptionCreation>
+      list={[{ value: 'enabled', title: 'Enabled' }, { value: 'disabled', title: 'Disabled' }]}
+      value={autoSubscriptionCreation ? 'enabled' : 'disabled'}
+      onChange={async (v) => {
+        if (v === 'enabled') {
+          await adminClient.namespaces.setAutoSubscriptionCreation(props.tenant, props.namespace, { allowAutoSubscriptionCreation: true });
+        } else {
+          await adminClient.namespaces.removeAutoSubscriptionCreation(props.tenant, props.namespace);
+        }
+
+        await mutate(swrKey);
+      }}
+    />
+  );
+}
+
+const field = (props: FieldInputProps): ConfigurationField => ({
+  id: policyId,
+  title: 'Auto subscription creation',
+  description: <span>Enable or disable auto subscription creation.</span>,
+  input: <FieldInput {...props} />
+});
+
+export default field;
