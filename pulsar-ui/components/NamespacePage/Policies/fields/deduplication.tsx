bf813948d072c25543f5aaa75f547ed853f6ac90
visortelle <visortelle@gmail.com>
2022-06-14 08:53:57 +0200

    Minor changes

--- pulsar-ui/components/NamespacePage/Policies/fields/deduplication.tsx
+++ pulsar-ui/components/NamespacePage/Policies/fields/deduplication.tsx
@@ -32,7 +32,7 @@ export const FieldInput: React.FC<FieldInputProps> = (props) => {
 
   return (
     <SelectInput<Deduplication>
-      list={[{ value: 'enabled', title: 'Enabled' }, { value: 'disabled', title: 'Disabled' }]}
+      list={[{ value: 'disabled', title: 'Disabled' }, { value: 'enabled', title: 'Enabled' }]}
       value={deduplication ? 'enabled' : 'disabled'}
       onChange={async (v) => {
         if (v === 'enabled') {
