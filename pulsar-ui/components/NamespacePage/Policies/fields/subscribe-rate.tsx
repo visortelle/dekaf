77e136d495c84d490226ced53b6db2ccf5921917
visortelle <visortelle@gmail.com>
2022-06-14 09:08:57 +0200

    Impelment subscribe rate field

---
+++ pulsar-ui/components/NamespacePage/Policies/fields/subscribe-rate.tsx
@@ -0,0 +1,139 @@
+import * as Notifications from '../../../contexts/Notifications';
+import * as PulsarAdminClient from '../../../contexts/PulsarAdminClient';
+import useSWR, { useSWRConfig } from "swr";
+import { ConfigurationField } from "../../../ConfigurationTable/ConfigurationTable";
+import sf from '../../../ConfigurationTable/form.module.css';
+import Input from "../../../ConfigurationTable/Input/Input";
+import { useEffect, useState } from 'react';
+import UpdateConfirmation from '../../../ConfigurationTable/UpdateConfirmation/UpdateConfirmation';
+import SelectInput from "../../../ConfigurationTable/SelectInput/SelectInput";
+
+const policyId = 'subscribeRate';
+
+export type SubscribeRate = 'disabled' | {
+  subscribeRate: number;
+  subscribeRatePeriod: number;
+}
+
+const defaultSubscribeRate: SubscribeRate = {
+  subscribeRate: -1,
+  subscribeRatePeriod: 30,
+}
+
+export type SubscribeRateInputProps = {
+  value: SubscribeRate;
+  onChange: (value: SubscribeRate) => void;
+};
+export const ReplicatorDispatchRateInput: React.FC<SubscribeRateInputProps> = (props) => {
+  const [subscribeRate, setSubscribeRate] = useState<SubscribeRate>(props.value);
+
+  useEffect(() => {
+    setSubscribeRate(() => props.value);
+  }, [props.value]);
+
+  const showUpdateConfirmation = JSON.stringify(props.value) !== JSON.stringify(subscribeRate);
+
+  return (
+    <div>
+      <div className={sf.FormItem}>
+        <SelectInput<'enabled' | 'disabled'>
+          list={[{ value: 'disabled', title: 'Disabled' }, { value: 'enabled', title: 'Enabled' }]}
+          value={subscribeRate === 'disabled' ? 'disabled' : 'enabled'}
+          onChange={(value) => setSubscribeRate(value === 'disabled' ? 'disabled' : defaultSubscribeRate)}
+        />
+      </div>
+
+      {subscribeRate !== 'disabled' && (
+        <div>
+          <div className={sf.FormItem}>
+            <strong className={sf.FormLabel}>Subscribe rate</strong>
+            <Input
+              type='number'
+              onChange={(v) => setSubscribeRate({ ...subscribeRate, subscribeRate: Number(v) })}
+              value={String(subscribeRate.subscribeRate)}
+            />
+          </div>
+
+          <div className={sf.FormItem}>
+            <strong className={sf.FormLabel}>Subscribe rate period (sec.)</strong>
+            <Input
+              type='number'
+              onChange={(v) => setSubscribeRate({ ...subscribeRate, subscribeRatePeriod: Number(v) })}
+              value={String(subscribeRate.subscribeRatePeriod)}
+            />
+          </div>
+        </div>
+      )}
+
+      {showUpdateConfirmation && (
+        <UpdateConfirmation
+          onUpdate={() => props.onChange(subscribeRate)}
+          onReset={() => setSubscribeRate(props.value)}
+        />
+      )}
+    </div>
+  );
+}
+
+export declare type SubscribeRateData = {
+  subscribeThrottlingRatePerConsumer?: number;
+  ratePeriodInSecond?: number;
+};
+
+export type FieldInputProps = {
+  tenant: string;
+  namespace: string;
+}
+
+export const FieldInput: React.FC<FieldInputProps> = (props) => {
+  const adminClient = PulsarAdminClient.useContext().client;
+  const { notifyError } = Notifications.useContext();
+  const { mutate } = useSWRConfig();
+
+  const onUpdateError = (err: string) => notifyError(`Can't update subscribe rate. ${err}`);
+  const swrKey = ['pulsar', 'tenants', props.tenant, 'namespaces', props.namespace, 'policies', policyId];
+
+  const { data: subscribeRateData, error: subscribeRateError } = useSWR(
+    swrKey,
+    async () => await adminClient.namespaces.getSubscribeRate(props.tenant, props.namespace) as unknown as SubscribeRateData
+  );
+
+  if (subscribeRateError) {
+    notifyError(`Unable to get subscribe rate. ${subscribeRateError}`);
+  }
+
+  const subscribeRate: SubscribeRate = subscribeRateData === undefined ? 'disabled' : {
+    subscribeRate: subscribeRateData.subscribeThrottlingRatePerConsumer === undefined ? defaultSubscribeRate.subscribeRate : subscribeRateData.subscribeThrottlingRatePerConsumer,
+    subscribeRatePeriod: subscribeRateData.ratePeriodInSecond === undefined ? defaultSubscribeRate.subscribeRatePeriod : subscribeRateData.ratePeriodInSecond,
+  }
+
+  return (
+    <ReplicatorDispatchRateInput
+      value={subscribeRate}
+      onChange={async (v) => {
+        if (v === 'disabled') {
+          await adminClient.namespaces.deleteSubscribeRate(props.tenant, props.namespace);
+        } else {
+          await adminClient.namespaces.setSubscribeRate(
+            props.tenant,
+            props.namespace,
+            {
+              subscribeThrottlingRatePerConsumer: v.subscribeRate,
+              ratePeriodInSecond: v.subscribeRatePeriod,
+            }).catch(onUpdateError);
+        }
+
+        await mutate(swrKey);
+      }}
+    />
+  )
+}
+
+const field = (props: FieldInputProps): ConfigurationField => ({
+  id: policyId,
+  title: 'Subscribe rate',
+  description: <span>Set subscribe-rate per consumer for all topics of the namespace.</span>,
+  input: <FieldInput {...props} />
+});
+
+export default field;
