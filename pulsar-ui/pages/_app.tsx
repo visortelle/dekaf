0be050dcb83747d1af62fbde8bbd13f4edaa21e1
visortelle <visortelle@gmail.com>
2022-06-15 20:40:00 +0200

    Provide broker config info

--- pulsar-ui/pages/_app.tsx
+++ pulsar-ui/pages/_app.tsx
@@ -2,19 +2,25 @@ import type { AppProps } from 'next/app'
 import * as AsyncTasks from '../components/contexts/AsyncTasks';
 import * as Notifications from '../components/contexts/Notifications';
 import * as PulsarAdminClient from '../components/contexts/PulsarAdminClient';
+import * as BrokerConfig from '../components/contexts/BrokerConfig';
 import 'react-toastify/dist/ReactToastify.css';
 import NoSsr from '../components/NoSsr/NoSsr';
+import { SWRConfig } from 'swr';
 
 const MyApp = (props: AppProps) => {
   return (
     <NoSsr>
-      <AsyncTasks.DefaultProvider>
-        <Notifications.DefaultProvider>
-          <PulsarAdminClient.DefaultProvider>
-            {typeof window === 'undefined' ? null : <props.Component />}
-          </PulsarAdminClient.DefaultProvider>
-        </Notifications.DefaultProvider>
-      </AsyncTasks.DefaultProvider>
+      <SWRConfig value={{ shouldRetryOnError: false, focusThrottleInterval: 120 }}>
+        <AsyncTasks.DefaultProvider>
+          <Notifications.DefaultProvider>
+            <PulsarAdminClient.DefaultProvider>
+              <BrokerConfig.DefaultProvider>
+                {typeof window === 'undefined' ? null : <props.Component />}
+              </BrokerConfig.DefaultProvider>
+            </PulsarAdminClient.DefaultProvider>
+          </Notifications.DefaultProvider>
+        </AsyncTasks.DefaultProvider>
+      </SWRConfig>
     </NoSsr>
   );
 }
