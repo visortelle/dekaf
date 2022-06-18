28da0b6388b020456b038c811ff020c8bfd78790
visortelle <visortelle@gmail.com>
2022-06-18 21:01:50 +0200

    Preserve nav tree query in search params

---
+++ pulsar-ui/components/ui/LinkWithQuery/LinkWithQuery.tsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
+
+const paramsToPreserve = ['q'];
+
+// Link preserving global search query and other global query params.
+const LinkWithQuery = (props: Parameters<typeof Link>[0]) => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const p = paramsToPreserve
+    .map(param => {
+      const value = queryParams.get(param);
+      if (value === null) {
+        return undefined;
+      }
+      return `${param}=${value}`;
+    })
+    .filter(p => p !== undefined)
+    .join('&');
+
+  return (
+    <Link {...props} to={`${props.to}?${p}`} />
+  );
+};
+
+export default LinkWithQuery;
