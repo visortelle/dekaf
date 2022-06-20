849598f9461e02f3d8c46c1457ed442f1b6f322a
visortelle <visortelle@gmail.com>
2022-06-20 16:19:45 +0200

    Experimental: implement PulsarAdminBatchClient

--- pulsar-ui/pages/_app.tsx
+++ pulsar-ui/pages/_app.tsx
@@ -2,10 +2,11 @@ import type { AppProps } from 'next/app'
 import * as AsyncTasks from '../components/app/contexts/AsyncTasks';
 import * as Notifications from '../components/app/contexts/Notifications';
 import * as PulsarAdminClient from '../components/app/contexts/PulsarAdminClient';
+import * as PulsarAdminBatchClient from '../components/app/contexts/PulsarAdminBatchClient/PulsarAdminBatchClient';
 import * as BrokerConfig from '../components/app/contexts/BrokersConfig';
 import 'react-toastify/dist/ReactToastify.css';
 import NoSsr from '../components/ui/NoSsr/NoSsr';
-import { SWRConfig, SWRHook } from 'swr';
+import { SWRConfig } from 'swr';
 import fetchIntercept from 'fetch-intercept';
 import { useEffect } from 'react';
 import stringify from 'safe-stable-stringify';
@@ -47,9 +48,11 @@ const _MyApp = (props: AppProps) => {
       <NoSsr>
         <Notifications.DefaultProvider>
           <PulsarAdminClient.DefaultProvider>
+          <PulsarAdminBatchClient.DefaultProvider>
             <BrokerConfig.DefaultProvider>
               {typeof window === 'undefined' ? null : <props.Component />}
             </BrokerConfig.DefaultProvider>
+            </PulsarAdminBatchClient.DefaultProvider>
           </PulsarAdminClient.DefaultProvider>
         </Notifications.DefaultProvider>
       </NoSsr >
