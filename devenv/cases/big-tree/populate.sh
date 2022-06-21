4f5194d29da670c34c3d6bfb1d180fb7f9338189
visortelle <visortelle@gmail.com>
2022-06-21 11:25:30 +0200

    Change big-tree devenv case params

--- devenv/cases/big-tree/populate.sh
+++ devenv/cases/big-tree/populate.sh
@@ -2,22 +2,22 @@
 
 api_url="http://localhost:8080/admin/v2"
 
-tenants_count=100
-namespaces_count=100
-topics_count=2
+tenants_count=1000
+namespaces_count=10
+topics_count=100
 
 for te in $(seq $tenants_count); do
   curl -X PUT \
     -H 'Content-Type: application/json' \
     -d '{"allowedClusters": ["standalone"], "adminRoles": [""]}' \
-    "${api_url}/tenants/tenant-${te}" &
+    "${api_url}/tenants/tenant-${te}"
 done
 
 for te in $(seq $tenants_count); do
   for ns in $(seq $namespaces_count); do
     curl -X PUT \
       -H 'Content-Type: application/json' \
-      "${api_url}/namespaces/tenant-${te}/namespace-${ns}" &
+      "${api_url}/namespaces/tenant-${te}/namespace-${ns}"
   done
 done
 
