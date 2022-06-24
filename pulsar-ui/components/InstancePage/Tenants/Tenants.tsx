cba1d4f70838df5be1ec1f04fbe529452782e644
visortelle <visortelle@gmail.com>
2022-06-24 15:11:35 +0200

    Implement custom api client

--- pulsar-ui/components/InstancePage/Tenants/Tenants.tsx
+++ pulsar-ui/components/InstancePage/Tenants/Tenants.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import s from './Tenants.module.css'
 import * as PulsarAdminClient from '../../app/contexts/PulsarAdminClient';
-import * as PulsarBrokerStatsClient from '../../app/contexts/PulsarBrokerStatsClient/PulsarBrokerStatsClient';
+import * as PulsarCustomApiClient from '../../app/contexts/PulsarCustomApiClient/PulsarCustomApiClient';
 import * as Notifications from '../../app/contexts/Notifications';
 import useSWR from 'swr';
 import { swrKeys } from '../../swrKeys';
 import { TableVirtuoso } from 'react-virtuoso';
-import { MetricsMap, Metric } from '../../../pages/api/broker-stats/metrics';
+import { TenantMetrics } from 'tealtools-pulsar-ui-api/metrics/types';
 
 export type TenantsProps = {};
 
 const Tenants: React.FC<TenantsProps> = (props) => {
   const adminClient = PulsarAdminClient.useContext().client;
-  const brokerStatsClient = PulsarBrokerStatsClient.useContext().client;
+  const customApiClient = PulsarCustomApiClient.useContext().client;
   const { notifyError } = Notifications.useContext();
 
   const { data: tenants, error: tenantsError } = useSWR(
@@ -23,13 +23,13 @@ const Tenants: React.FC<TenantsProps> = (props) => {
     notifyError(`Unable to get tenants list. ${tenantsError}`);
   }
 
-  const { data: metrics, error: metricsError } = useSWR(
+  const { data: allTenantsMetrics, error: allTenantsMetricsError } = useSWR(
     ['abc'],
-    // async () => await brokerStatsClient.getMetrics({ type: 'by-dimensions', filter: { "tenant-1": { "namespace": "tenant-1/namespace-1" } } }),
-    async () => await brokerStatsClient.getMetrics({ type: 'all-tenants' }) as unknown as Record<string, TenantMetricSchema>,
+    async () => await customApiClient.getAllTenantsMetrics(),
+    {refreshInterval: 10 * 1000}
   );
-  if (metricsError) {
-    notifyError(`Unable to get metrics. ${metricsError}`);
+  if (allTenantsMetricsError) {
+    notifyError(`Unable to get metrics. ${allTenantsMetricsError}`);
   }
 
   return (
@@ -39,7 +39,7 @@ const Tenants: React.FC<TenantsProps> = (props) => {
         overscan={{ main: window.innerHeight / 2, reverse: window.innerHeight / 2 }}
         itemContent={(_, tenant) => {
 
-        const tenantMetrics = (metrics || {})[tenant];
+        const tenantMetrics = (allTenantsMetrics || {})[tenant];
         return <Tenant tenant={tenant} metrics={tenantMetrics}/>
       }}
         useWindowScroll={true}
@@ -51,16 +51,15 @@ const Tenants: React.FC<TenantsProps> = (props) => {
 
 type TenantProps = {
   tenant: string;
-  metrics: TenantMetricSchema | undefined
+  metrics: TenantMetrics
 }
 const Tenant: React.FC<TenantProps> = (props) => {
   return (
     <>
       <td>{props.tenant}</td>
-      <td>{withDefault(props.metrics?.brk_ml_AddEntryWithReplicasBytesRate, '')}</td>
-      <td>{withDefault(props.metrics?.brk_ml_ReadEntriesRate, '')}</td>
-      <td>{withDefault(props.metrics?.brk_ml_AddEntryBytesRate, '')}</td>
-      <td>{withDefault(props.metrics?.brk_ml_AddEntryBytesRate, '')}</td>
+      <td>{withDefault(props.metrics?.averageMsgSize, '')}</td>
+      <td>{withDefault(props.metrics?.bytesInCount, '')}</td>
+      <td>{withDefault(props.metrics?.msgInCount, '')}</td>
     </>
   );
 }
@@ -70,72 +69,3 @@ export default Tenants;
 function withDefault(value: any, defaultValue: any) {
   return value === undefined ? defaultValue : value;
 }
-
-type TenantMetrics = {
-  inRateBytes: number | undefined,
-  outRateBytes: number | undefined,
-  inThroughputBytes: number | undefined,
-  outThroughputBytes: number | undefined,
-  storageSizeBytes: number | undefined,
-  messagesInBacklog: number | undefined,
-}
-
-type TenantMetricSchema = {
-  "brk_ml_AddEntryBytesRate": number | undefined,
-  "brk_ml_AddEntryErrors": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_0.0_0.5": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_0.5_1.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_1.0_5.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_10.0_20.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_100.0_200.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_20.0_50.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_200.0_1000.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_5.0_10.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_50.0_100.0": number | undefined,
-  "brk_ml_AddEntryLatencyBuckets_OVERFLOW": number | undefined,
-  "brk_ml_AddEntryMessagesRate": number | undefined,
-  "brk_ml_AddEntrySucceed": number | undefined,
-  "brk_ml_AddEntryWithReplicasBytesRate": number | undefined,
-  "brk_ml_EntrySizeBuckets_0.0_128.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_1024.0_2048.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_102400.0_1048576.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_128.0_512.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_16384.0_102400.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_2048.0_4096.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_4096.0_16384.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_512.0_1024.0": number | undefined,
-  "brk_ml_EntrySizeBuckets_OVERFLOW": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_0.0_0.5": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_0.5_1.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_1.0_5.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_10.0_20.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_100.0_200.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_20.0_50.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_200.0_1000.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_5.0_10.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_50.0_100.0": number | undefined,
-  "brk_ml_LedgerAddEntryLatencyBuckets_OVERFLOW": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_0.0_0.5": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_0.5_1.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_1.0_5.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_10.0_20.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_100.0_200.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_20.0_50.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_200.0_1000.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_5.0_10.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_50.0_100.0": number | undefined,
-  "brk_ml_LedgerSwitchLatencyBuckets_OVERFLOW": number | undefined,
-  "brk_ml_MarkDeleteRate": number | undefined,
-  "brk_ml_NumberOfMessagesInBacklog": number | undefined,
-  "brk_ml_ReadEntriesBytesRate": number | undefined,
-  "brk_ml_ReadEntriesErrors": number | undefined,
-  "brk_ml_ReadEntriesRate": number | undefined,
-  "brk_ml_ReadEntriesSucceeded": number | undefined,
-  "brk_ml_StoredMessagesSize": number | undefined
-}
-
-// const getTenantMetrics(metrics: TenantMetricSchema): TenantMetrics {
-//   return {
-
-//   }
-// }
---
+++ pulsar-ui/components/app/contexts/PulsarCustomApiClient/client/client.ts
@@ -0,0 +1,62 @@
+import stringify from "safe-stable-stringify";
+import * as brokerMetrics from "tealtools-pulsar-ui-api/broker-metrics/broker-metrics";
+import * as metrics from "tealtools-pulsar-ui-api/metrics/types";
+
+export type ClientConfig = {
+  apiUrl: string;
+  notifyError: (message: string) => void;
+};
+
+export type Client = {
+  getTenantMetrics: (tenant: string) => Promise<metrics.TenantMetrics>;
+  getAllTenantsMetrics: () => Promise<Record<string, metrics.TenantMetrics>>;
+  getMetrics: (filters: brokerMetrics.Filter) => Promise<brokerMetrics.MetricsMap<any>>;
+};
+
+export const dummyClient: Client = {
+  getAllTenantsMetrics: async () => ({}),
+  getMetrics: async () => ({}),
+  getTenantMetrics: async () => ({}),
+}
+
+export function createClient(config: ClientConfig): Client {
+  return {
+    getTenantMetrics: (tenant) => getTenantMetrics(config, tenant),
+    getAllTenantsMetrics: () => getAllTenantsMetrics(config),
+    getMetrics: (filter) => getMetrics(config, filter),
+  };
+}
+
+async function getTenantMetrics(
+  config: ClientConfig,
+  tenant: string
+): Promise<metrics.TenantMetrics> {
+  const res = await fetch(`${config.apiUrl}/metrics/tenants/${tenant}`);
+  return res.json();
+}
+
+async function getAllTenantsMetrics(
+  config: ClientConfig
+): Promise<Record<string, metrics.TenantMetrics>> {
+  const res = await fetch(`${config.apiUrl}/metrics/allTenants`);
+  return res.json();
+}
+
+async function getMetrics(config: ClientConfig, filter: brokerMetrics.Filter): Promise<brokerMetrics.MetricsMap<any>> {
+  const response = await fetch(
+    `${config.apiUrl}/broker-stats/metrics?filter=${stringify(filter)}`,
+    {}
+  ).catch((error) => console.error(error));
+  if (response === undefined) {
+    config.notifyError(`Unable to get metrics.`);
+    return {};
+  }
+
+  const json = await response.json();
+  if (response.status >= 300) {
+    config.notifyError(`Unable to get metrics: ${json.error!}`);
+    return {};
+  }
+
+  return json as brokerMetrics.MetricsMap<any>;
+};
