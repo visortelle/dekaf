145d53dcb24530adc6381a0a0809a70cfd03ea77
visortelle <visortelle@gmail.com>
2022-07-07 14:04:23 +0200

    Stream topic messages at scala-api

--- api-scala/src/main/scala/Main.scala
+++ api-scala/src/main/scala/Main.scala
@@ -1,25 +1,49 @@
 import org.apache.pulsar.client.api.Consumer
 import org.apache.pulsar.client.api.PulsarClient
 import org.apache.pulsar.client.api.MessageListener
-import tools.teal.pulsar.ui.api.messages
+import tools.teal.pulsar.ui.api.topic
+import tools.teal.pulsar.ui.api.topic.ReadMessagesRequest
+import tools.teal.pulsar.ui.api.topic.ReadMessagesResponse
+import io.grpc.{Server, ServerBuilder}
+import scala.concurrent.{ExecutionContext, Future}
+import io.grpc.stub.StreamObserver
+import io.grpc.protobuf.services.ProtoReflectionService
+import scala.jdk.CollectionConverters.*
+import com.google.protobuf.ByteString
 
-@main def hello: Unit =
+@main def main: Unit =
   println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
+  server.start
+  server.awaitTermination
 
 val client = PulsarClient.builder().serviceUrl("pulsar://localhost:6650").build()
 
-val listener: MessageListener[Any] = (consumer, msg) => {
-  println("Message received: " ++ msg.getData)
-  consumer.acknowledge(msg)
-}
+private class TopicServiceImpl extends topic.TopicServiceGrpc.TopicService:
+  override def readMessages(request: ReadMessagesRequest, responseObserver: StreamObserver[ReadMessagesResponse]): Unit =
+    val listener: MessageListener[Array[Byte]] = (consumer, msg) => {
+      println("Message received: " + msg.getData)
 
-val consumer = client.newConsumer()
-  .topic("persistent://tenant-1/namespace-1/topic-1")
-  .subscriptionName("test-subscription")
-  .subscribe()
+      val message = topic.Message(
+        topic = request.topic,
+        producerName = msg.getProducerName,
+        properties = msg.getProperties.asScala.toMap,
+        data = ByteString.copyFrom(msg.getData),
+      )
+      responseObserver.onNext(ReadMessagesResponse(messages = Seq(message)))
+      consumer.acknowledge(msg)
+    }
+
+    val consumer = client.newConsumer()
+      .topic(request.topic)
+      .subscriptionName("test-subscription")
+      .messageListener(listener)
+      .subscribe()
+
+val server = ServerBuilder
+  .forPort(8090)
+  .addService(topic.TopicServiceGrpc.bindService(TopicServiceImpl(), ExecutionContext.global))
+  .addService(ProtoReflectionService.newInstance)
+  .build
 
 def consume(tenant: String, namespace: String, topicType: "persistent" | "non-persistent", topic: String): Unit =
   ()
