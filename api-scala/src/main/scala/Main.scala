665857cda05dac581a2d45546a65eae1e042d246
visortelle <visortelle@gmail.com>
2022-07-08 16:54:58 +0200

    In progress consumer gRPC service

--- api-scala/src/main/scala/Main.scala
+++ api-scala/src/main/scala/Main.scala
@@ -1,6 +1,11 @@
 import org.apache.pulsar.client.api.{ Consumer, PulsarClient, MessageListener }
-import tools.teal.pulsar.ui.api.topic
-import tools.teal.pulsar.ui.api.topic.{ ReadMessagesRequest, ReadMessagesResponse }
+import com.tools.teal.pulsar.ui.api.v1.consumer.{
+  ConsumerServiceGrpc,
+  CreateConsumerRequest,
+  CreateConsumerResponse,
+  DeleteConsumerRequest,
+  DeleteConsumerResponse,
+}
 import io.grpc.{Server, ServerBuilder}
 import scala.concurrent.{ExecutionContext, Future}
 import io.grpc.stub.StreamObserver
@@ -9,46 +14,50 @@ import scala.jdk.CollectionConverters.*
 import com.google.protobuf.ByteString
 import org.apache.pulsar.client.api.{SubscriptionMode, SubscriptionType}
 import org.apache.pulsar.client.api.SubscriptionInitialPosition
+import org.apache.pulsar.client.api.RegexSubscriptionMode
 
-@main def main: Unit =
-  println("Starting Pulsar X-Ray server")
-  server.start
-  server.awaitTermination
+// @main def main: Unit =
+//   println("Starting Pulsar X-Ray server")
+//   server.start
+//   server.awaitTermination
 
 val client =
   PulsarClient.builder().serviceUrl("pulsar://localhost:6650").build()
 
-private class TopicServiceImpl extends topic.TopicServiceGrpc.TopicService:
+private class ConsumerServiceImpl extends ConsumerServiceGrpc.ConsumerService:
   override def readMessages(
-      request: ReadMessagesRequest,
-      responseObserver: StreamObserver[ReadMessagesResponse]
+      // request: ReadMessagesRequest,
+      // responseObserver: StreamObserver[ReadMessagesResponse]
   ): Unit =
-    val listener: MessageListener[Array[Byte]] = (consumer, msg) => {
-      val message = topic.Message(
-        topic = request.topic,
-        producerName = msg.getProducerName,
-        properties = msg.getProperties.asScala.toMap,
-        data = ByteString.copyFrom(msg.getData),
-        messageId = ByteString.copyFrom(msg.getMessageId.toByteArray)
-      )
-      responseObserver.onNext(ReadMessagesResponse(messages = Seq(message)))
-      consumer.acknowledge(msg)
-    }
+    // val listener: MessageListener[Array[Byte]] = (consumer, msg) => {
+    //   val message = topic.Message(
+    //     topic = request.topic,
+    //     producerName = msg.getProducerName,
+    //     properties = msg.getProperties.asScala.toMap,
+    //     data = ByteString.copyFrom(msg.getData),
+    //     messageId = ByteString.copyFrom(msg.getMessageId.toByteArray)
+    //   )
+    //   responseObserver.onNext(ReadMessagesResponse(messages = Seq(message)))
+    //   consumer.acknowledge(msg)
+    // }
 
     val consumer = client
       .newConsumer
-      .topic(request.topic)
-      .subscriptionName("__xray-" + request.subscriptionId)
+      // .consumerName(request.consumerName)
+      // .subscriptionMode()
+      // .topic(request.topic)
+      // .subscriptionName(request.subscriptionId)
       .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
-      .messageListener(listener)
-      .subscriptionType(SubscriptionType.Shared)
+      // .messageListener(listener)
+      // .subscriptionType(request.subscriptionType)
+      // .startPaused(request.startPaused)
       .subscribe
 
-val server = ServerBuilder
-  .forPort(8090)
-  .addService(
-    topic.TopicServiceGrpc
-      .bindService(TopicServiceImpl(), ExecutionContext.global)
-  )
-  .addService(ProtoReflectionService.newInstance)
-  .build
+// val server = ServerBuilder
+//   .forPort(8090)
+//   .addService(
+//     topic.TopicServiceGrpc
+//       .bindService(ConsumerServiceImpl(), ExecutionContext.global)
+//   )
+//   .addService(ProtoReflectionService.newInstance)
+//   .build
