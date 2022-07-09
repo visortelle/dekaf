14347bba070a9740c94770bf86395aea6779c575
visortelle <visortelle@gmail.com>
2022-07-09 13:39:46 +0200

    Implement start/pause for consumer

--- pulsar-ui/components/TopicPage/Messages/Messages.tsx
+++ pulsar-ui/components/TopicPage/Messages/Messages.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import s from './Messages.module.css'
 import * as PulsarGrpcClient from '../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
-import { Message, CreateConsumerRequest, ResumeRequest, ResumeResponse, SubscriptionType, TopicSelector, DeleteConsumerRequest } from '../../../grpc-web/tools/teal/pulsar/ui/api/v1/consumer_pb';
+import { Message, CreateConsumerRequest, ResumeRequest, ResumeResponse, SubscriptionType, TopicSelector, DeleteConsumerRequest, PauseRequest } from '../../../grpc-web/tools/teal/pulsar/ui/api/v1/consumer_pb';
 import { nanoid } from 'nanoid';
 import * as Notifications from '../../app/contexts/Notifications';
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso';
@@ -28,17 +28,13 @@ const Messages: React.FC<MessagesProps> = (props) => {
   const { consumerServiceClient } = PulsarGrpcClient.useContext();
   const [messages, setMessages] = useState<KeyedMessage[]>([]);
   const [isFollowOutput, setIsFollowOutput] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
   const [consumerName, _] = useState('__xray_' + nanoid());
   const [subscriptionName, __] = useState('__xray_' + nanoid());
+  const [stream, setStream] = useState<ClientReadableStream<ResumeResponse>>();
 
   useEffect(() => {
-    let stream: ClientReadableStream<ResumeResponse>;
-
     async function startConsume() {
-      if (stream) {
-        return;
-      }
-
       const createConsumerReq = new CreateConsumerRequest();
       const topicSelector = new TopicSelector();
       topicSelector.setTopic(`${props.topicType}://${props.tenant}/${props.namespace}/${props.topic}`);
@@ -61,11 +57,7 @@ const Messages: React.FC<MessagesProps> = (props) => {
       const resumeReq = new ResumeRequest();
       resumeReq.setConsumerName(consumerName);
 
-      stream = consumerServiceClient.resume(resumeReq);
-      stream.on('data', (data) => {
-        const newMessages = data.getMessagesList().map(m => ({ message: m, key: nanoid() }));
-        setMessages(messages => messages.concat(newMessages));
-      });
+      setStream(() => consumerServiceClient.resume(resumeReq));
     }
 
     startConsume();
@@ -83,6 +75,17 @@ const Messages: React.FC<MessagesProps> = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (stream === undefined) {
+      return;
+    }
+
+    stream.on('data', (data) => {
+      const newMessages = data.getMessagesList().map(m => ({ message: m, key: nanoid() }));
+      setMessages(messages => messages.concat(newMessages));
+    });
+  }, [stream]);
+
   return (
     <div className={s.Messages}>
       <div>
@@ -96,7 +99,25 @@ const Messages: React.FC<MessagesProps> = (props) => {
           }}
           type='primary'
         />
-        <SmallButton text="Stop" onClick={() => setIsFollowOutput(!isFollowOutput)} type='danger' />
+        <SmallButton
+          text={isPaused ? "Resume" : "Pause"}
+          onClick={async () => {
+            if (isPaused) {
+              setIsPaused(false);
+              const resumeReq = new ResumeRequest();
+              resumeReq.setConsumerName(consumerName);
+              stream?.cancel();
+              const newStream = consumerServiceClient.resume(resumeReq, { deadline: createDeadline(10) });
+              setStream(() => newStream);
+            } else {
+              setIsPaused(true);
+              const pauseReq = new PauseRequest();
+              pauseReq.setConsumerName(consumerName);
+              consumerServiceClient.pause(pauseReq, { deadline: createDeadline(10) });
+            }
+          }}
+          type='danger'
+        />
         <strong>{messages.length}</strong> messages loaded
       </div>
       <div className={s.List} ref={listRef}>
