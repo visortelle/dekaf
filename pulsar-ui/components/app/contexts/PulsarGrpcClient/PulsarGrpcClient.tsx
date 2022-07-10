96ac7c007dba16f032a66c240fcc1ce3863f749a
visortelle <visortelle@gmail.com>
2022-07-10 16:13:28 +0200

    Better topic messages stream request cancelation

--- pulsar-ui/components/app/contexts/PulsarGrpcClient/PulsarGrpcClient.tsx
+++ pulsar-ui/components/app/contexts/PulsarGrpcClient/PulsarGrpcClient.tsx
@@ -14,16 +14,6 @@ const Context = React.createContext<Value>(defaultValue);
 export const DefaultProvider = ({ children }: { children: ReactNode }) => {
   const [consumerServiceClient, _] = useState<_consumerServiceClient.ConsumerServiceClient>(new _consumerServiceClient.ConsumerServiceClient('http://localhost:10000'));
 
-  // useEffect(() => {
-  //   if (!window) {
-  //     return;
-  //   }
-
-  //   // Use https://github.com/SafetyCulture/grpc-web-devtools
-  //   const enableDevTools = (window as any).__GRPCWEB_DEVTOOLS__ || (() => { });
-  //   enableDevTools([consumerServiceClient]);
-  // }, [consumerServiceClient]);
-
   return (
     <>
       <Context.Provider
