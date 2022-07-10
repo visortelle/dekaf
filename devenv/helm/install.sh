55eb3d7cbc44e28e8b4f502f756e08a3c95aa4c8
visortelle <visortelle@gmail.com>
2022-07-10 12:19:36 +0200

    Upgrade devenv Pulsar to 2.10.1

--- devenv/helm/install.sh
+++ devenv/helm/install.sh
@@ -8,10 +8,9 @@ ns="pulsar-devenv"
 helm_release="pulsar-devenv"
 
 rm -rf "${this_dir}/pulsar-helm-chart"
-git clone --depth 1 https://github.com/apache/pulsar-helm-chart -b pulsar-2.9.2 --single-branch "${this_dir}/pulsar-helm-chart"
+git clone --depth 1 https://github.com/tealtools/pulsar-helm-chart -b tealtools-fixes --single-branch "${this_dir}/pulsar-helm-chart"
 "${this_dir}/pulsar-helm-chart/scripts/pulsar/prepare_helm_release.sh" -n "${ns}" -k "${helm_release}"
+
+helm upgrade "${helm_release}" "${this_dir}/pulsar-helm-chart/charts/pulsar" --install --force -n "${ns}" -f "${this_dir}/values.yaml"
+
 rm -rf "${this_dir}/pulsar-helm-chart"
-
-helm repo add apache https://pulsar.apache.org/charts
-
-helm upgrade "${helm_release}" apache/pulsar --install --force -n "${ns}" -f "${this_dir}/values.yaml"
