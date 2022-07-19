ce90ade9bd4de01e342de3517a73f4a86de19042
visortelle <visortelle@gmail.com>
2022-07-19 16:56:51 +0200

    Toggle consumer console

--- pulsar-ui/components/TopicPage/Messages/Console/Console.tsx
+++ pulsar-ui/components/TopicPage/Messages/Console/Console.tsx
@@ -3,8 +3,13 @@ import s from './Console.module.css'
 import SubscriptionsCursors from './SubscriptionsCursors/SubscriptionsCursors';
 import { SessionConfig, SessionState } from '../types';
 import { GetTopicsInternalStatsResponse } from '../../../../grpc-web/tools/teal/pulsar/ui/api/v1/topic_pb';
+import SvgIcon from '../../../ui/SvgIcon/SvgIcon';
+import closeIcon from '!!raw-loader!./close.svg';
+import EnteringFromBottomDiv from '../../../ui/animations/EnteringFromBottomDiv';
 
 export type ConsoleProps = {
+  isShow: boolean;
+  onClose: () => void;
   sessionKey: number;
   sessionSubscriptionName: string;
   sessionConfig: SessionConfig;
@@ -15,25 +20,36 @@ export type ConsoleProps = {
 
 const Console: React.FC<ConsoleProps> = (props) => {
   return (
-    <div className={s.Console}>
+    <EnteringFromBottomDiv className={s.Console} isVisible={props.isShow} motionKey='consumer-console'>
       <div className={s.Tabs}>
         <div className={s.Tab}>Cursors</div>
         {/* <div className={s.Tab}>Producer</div> */}
-      </div>
-      {props.sessionConfig.topicsSelector.type === 'by-names' && (
-        <div className={s.SubscriptionsCursors}>
-          <SubscriptionsCursors
-            sessionKey={props.sessionKey}
-            sessionSubscriptionName={props.sessionSubscriptionName}
-            sessionState={props.sessionState}
-            sessionConfig={props.sessionConfig}
-            onSessionStateChange={props.onSessionStateChange}
-            selector={props.sessionConfig.topicsSelector.topics.reduce((acc, topic) => ({ ...acc, [topic]: [props.sessionSubscriptionName] }), {})}
-            topicsInternalStats={props.topicsInternalStats}
-          />
+        <div className={s.CloseConsole} title="Close" onClick={props.onClose}>
+          <SvgIcon svg={closeIcon} />
         </div>
+      </div>
+      {!(props.sessionState === 'running' || props.sessionState === 'paused') && (
+        <div className={s.NothingToShow}>Run session to see cursors list.</div>
       )}
-    </div>
+      {props.sessionConfig.topicsSelector.type === 'by-names' && (
+        <>
+          <div className={s.SubscriptionsCursors} style={{
+            visibility: (props.sessionState === 'running' || props.sessionState === 'paused') ? 'visible' : 'hidden'
+          }}>
+            <SubscriptionsCursors
+              sessionKey={props.sessionKey}
+              sessionSubscriptionName={props.sessionSubscriptionName}
+              sessionState={props.sessionState}
+              sessionConfig={props.sessionConfig}
+              onSessionStateChange={props.onSessionStateChange}
+              selector={props.sessionConfig.topicsSelector.topics.reduce((acc, topic) => ({ ...acc, [topic]: [props.sessionSubscriptionName] }), {})}
+              topicsInternalStats={props.topicsInternalStats}
+            />
+          </div>
+        </>
+
+      )}
+    </EnteringFromBottomDiv>
   );
 }
 
---
+++ pulsar-ui/components/ui/animations/EnteringFromBottomDiv.tsx
@@ -0,0 +1,21 @@
+import { motion, AnimatePresence } from "framer-motion"
+
+const EnteringFromBottomDiv = ({ motionKey, children, className, isVisible }: { motionKey: string, children: React.ReactNode, className?: string, isVisible: boolean }) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={motionKey}
+        initial={{ y: isVisible ? 0 : '120%' }}
+        animate={{ y: isVisible ? 0 : '120%' }}
+        exit={{ y: '120%' }}
+        className={className || ''}
+        transition={{ duration: 0.33 }}
+        style={{ zIndex: isVisible ? 1 : 0 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default EnteringFromBottomDiv;
