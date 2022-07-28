f30465fe9ef62633d0f361496b7f5a6182359101
visortelle <visortelle@gmail.com>
2022-07-28 19:36:07 +0200

    Compile protobuf-native schema

--- pulsar-ui/components/TopicPage/Schema/ProtobufNativeEditor/ProtobufNativeEditor.tsx
+++ pulsar-ui/components/TopicPage/Schema/ProtobufNativeEditor/ProtobufNativeEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import s from './ProtobufNativeEditor.module.css'
 import * as Notifications from '../../../app/contexts/Notifications';
 import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
@@ -6,7 +6,8 @@ import uploadIcon from '!!raw-loader!./upload.svg';
 import SvgIcon from '../../../ui/SvgIcon/SvgIcon';
 import Upload from 'rc-upload';
 import Select from '../../../ui/Select/Select';
-import { CompileProtobufNativeRequest, FileEntry as FileEntryPb } from '../../../../grpc-web/tools/teal/pulsar/ui/api/v1/schema_pb';
+import { CompiledProtobufNativeFile, CompileProtobufNativeRequest, CompileProtobufNativeResponse, FileEntry as FileEntryPb } from '../../../../grpc-web/tools/teal/pulsar/ui/api/v1/schema_pb';
+import { divide } from 'lodash';
 
 export type ProtobufNativeEditorProps = {};
 
@@ -23,6 +24,10 @@ const ProtobufNativeEditor: React.FC<ProtobufNativeEditorProps> = (props) => {
   const { notifyError, notifySuccess } = Notifications.useContext();
   const [uploadState, setUploadState] = React.useState<UploadState>('awaiting');
   const [createFrom, setCreateFrom] = React.useState<CreateFrom>('directory');
+  const [files, setFiles] = React.useState<ReturnType<CompileProtobufNativeResponse['getFilesMap']> | undefined>(undefined);
+  const [selectedFile, setSelectedFile] = React.useState<string | undefined>(undefined);
+  const [selectedMessage, setSelectedMessage] = React.useState<string | undefined>(undefined);
+  const [compilationError, setCompilationError] = React.useState<string | undefined>(undefined);
   const { schemaServiceClient } = PulsarGrpcClient.useContext();
 
   const submitFiles = async (files: FileEntry[]) => {
@@ -36,17 +41,43 @@ const ProtobufNativeEditor: React.FC<ProtobufNativeEditorProps> = (props) => {
     const compileReq = new CompileProtobufNativeRequest();
     compileReq.setFilesList(filesPb);
 
-    const res = await schemaServiceClient.compileProtobufNative(compileReq, {});
-    console.log(res);
+    const res = await schemaServiceClient.compileProtobufNative(compileReq, {}).catch(err => notifyError(err));
+    if (res === undefined) {
+      return;
+    }
+
+    setFiles(() => res.getFilesMap());
+
+    const selectedFile = res.getFilesMap().getEntryList()[0][0];
+    setSelectedFile(selectedFile);
 
     setUploadState('awaiting');
-    notifySuccess('Schema successfully uploaded');
   }
+
+  useEffect(() => {
+    if (files === undefined || selectedFile === undefined) {
+      return;
+    }
+
+    const file = files.get(selectedFile);
+    if (file === undefined) {
+      return;
+    }
+
+    const compilationError = file.getCompilationError();
+    if (compilationError !== undefined) {
+      setCompilationError(compilationError);
+      setSelectedMessage(undefined);
+    }
+
+    const selectedMessage = file.getSchemasMap().getEntryList().map(([k]) => k)[0];
+    setSelectedMessage(selectedMessage);
+  }, [selectedFile]);
+
+  const fileNames = files === undefined ? [] : files.getEntryList().map(([k]) => k);
 
   return (
     <div className={s.ProtobufNativeEditor}>
-      protobuf native editor
-
       <div>
         <Select<CreateFrom>
           list={[
@@ -68,6 +99,45 @@ const ProtobufNativeEditor: React.FC<ProtobufNativeEditorProps> = (props) => {
           {createFrom === 'directory' && "Click here or drag'n'drop a directory with .proto files"}
         </UploadZone>
       )}
+
+      {fileNames.length > 0 && selectedFile !== undefined && (
+        <div>
+          <strong>File</strong>
+          <Select<string>
+            list={sortFilePaths(fileNames).map(name => ({ type: 'item', title: name, value: name }))}
+            value={selectedFile}
+            onChange={setSelectedFile}
+          />
+        </div>
+      )}
+      {selectedFile !== undefined && selectedMessage !== undefined && (
+        <div>
+          <strong>Message</strong>
+          <Select<string>
+            list={files?.get(selectedFile)?.getSchemasMap().getEntryList().map(([k]) => k).map(message => ({ type: 'item', title: message, value: message })) || []}
+            onChange={setSelectedMessage}
+            value={selectedMessage}
+          />
+        </div>
+      )}
+      {compilationError !== undefined && (
+        <div className={s.CompilationError}>
+          {compilationError.split('\n').map((line, i) => (<div key={i}>{line}</div>))}
+        </div>
+      )}
+
+      {selectedFile !== undefined && selectedMessage !== undefined && (
+        <div>
+          <strong>Schema</strong>
+          <textarea
+            rows={20}
+            cols={80}
+            spellCheck={false}
+            style={{ display: 'block'}}
+            value={files?.get(selectedFile)?.getSchemasMap().get(selectedMessage)?.getHumanReadableSchema()}
+          />
+        </div>
+      )}
     </div>
   );
 }
@@ -83,10 +153,10 @@ export const UploadZone: React.FC<UploadZoneProps> = (props) => {
     <Upload
       directory={props.isDirectory}
       beforeUpload={async (_, _files) => {
+        console.log('BEFORE');
         let files: FileEntry[] = [];
         for (let file of _files) {
           const content = await file.text();
-          console.log('files', _files);
           files.push({ relativePath: file.webkitRelativePath || file.name, content });
         }
 
@@ -99,6 +169,23 @@ export const UploadZone: React.FC<UploadZoneProps> = (props) => {
       </div>
     </Upload>
   );
+}
+
+export function sortFilePaths(filePaths: string[]): string[] {
+  const sep = "/";
+  return filePaths.sort((a, b) => {
+    const aPath = a.split(sep);
+    const bPath = b.split(sep);
+    const minLength = Math.min(aPath.length, bPath.length);
+    for (let i = 0; i < minLength; i++) {
+      const aPart = aPath[i];
+      const bPart = bPath[i];
+      if (aPart !== bPart) {
+        return aPart.localeCompare(bPart);
+      }
+    }
+    return aPath.length - bPath.length;
+  });
 }
 
 export default ProtobufNativeEditor;
