844e434a0ca95b0e5680ee748d6de57a0bbd3298
visortelle <visortelle@gmail.com>
2022-07-28 17:39:20 +0200

    Compile protobuf-native schema

--- pulsar-ui/components/TopicPage/Schema/ProtobufNativeEditor/ProtobufNativeEditor.tsx
+++ pulsar-ui/components/TopicPage/Schema/ProtobufNativeEditor/ProtobufNativeEditor.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import s from './ProtobufNativeEditor.module.css'
 import * as Notifications from '../../../app/contexts/Notifications';
+import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
+import uploadIcon from '!!raw-loader!./upload.svg';
+import SvgIcon from '../../../ui/SvgIcon/SvgIcon';
 import Upload from 'rc-upload';
-import { options } from 'numeral';
+import Select from '../../../ui/Select/Select';
+import { CompileProtobufNativeRequest, FileEntry as FileEntryPb } from '../../../../grpc-web/tools/teal/pulsar/ui/api/v1/schema_pb';
 
 export type ProtobufNativeEditorProps = {};
 
 type UploadState = 'awaiting' | 'uploading' | 'done' | 'error';
 
 type FileEntry = {
-  name: string,
   relativePath: string;
   content: string;
 }
@@ -19,9 +22,23 @@ type CreateFrom = 'text' | 'single-file' | 'directory';
 const ProtobufNativeEditor: React.FC<ProtobufNativeEditorProps> = (props) => {
   const { notifyError, notifySuccess } = Notifications.useContext();
   const [uploadState, setUploadState] = React.useState<UploadState>('awaiting');
-  const [createFrom, setCreateFrom] = React.useState<CreateFrom>('single-file');
+  const [createFrom, setCreateFrom] = React.useState<CreateFrom>('directory');
+  const { schemaServiceClient } = PulsarGrpcClient.useContext();
 
   const submitFiles = async (files: FileEntry[]) => {
+    const filesPb = files.map(file => {
+      const filePb = new FileEntryPb();
+      filePb.setRelativePath(file.relativePath);
+      filePb.setContent(file.content);
+      return filePb;
+    });
+
+    const compileReq = new CompileProtobufNativeRequest();
+    compileReq.setFilesList(filesPb);
+
+    const res = await schemaServiceClient.compileProtobufNative(compileReq, {});
+    console.log(res);
+
     setUploadState('awaiting');
     notifySuccess('Schema successfully uploaded');
   }
@@ -29,26 +46,58 @@ const ProtobufNativeEditor: React.FC<ProtobufNativeEditorProps> = (props) => {
   return (
     <div className={s.ProtobufNativeEditor}>
       protobuf native editor
-      {(createFrom === 'single-file' || createFrom === 'directory') && (
-        <Upload
-          directory={createFrom === 'directory'}
-          beforeUpload={async (_, _files) => {
-            let files: FileEntry[] = [];
-            for (let file of _files) {
-              const content = await file.text();
-              files.push({ name: file.name, relativePath: file.name, content });
-            }
 
-            submitFiles(files);
-            return false;
-          }}
-        >
-          <div className={s.UploadSchema}>
-            Upload directory
+      <div>
+        <Select<CreateFrom>
+          list={[
+            { type: 'item', title: 'Editor', value: 'text' },
+            { type: 'item', title: 'Single .proto file', value: 'single-file' },
+            { type: 'item', title: 'Directory with .proto files', value: 'directory' },
+          ]}
+          value={createFrom}
+          onChange={setCreateFrom}
+        />
+      </div>
+
+      {(createFrom === 'single-file' || createFrom === 'directory') && (
+        <UploadZone isDirectory={createFrom === 'directory'} onFiles={(files) => submitFiles(files)}>
+          <div className={s.UploadZoneIcon}>
+            <SvgIcon svg={uploadIcon} />
           </div>
-        </Upload>
+          {createFrom === 'single-file' && "Click here or drag'n'drop a .proto file"}
+          {createFrom === 'directory' && "Click here or drag'n'drop a directory with .proto files"}
+        </UploadZone>
       )}
     </div>
+  );
+}
+
+type UploadZoneProps = {
+  children: React.ReactNode,
+  onFiles: (files: FileEntry[]) => void,
+  isDirectory: boolean,
+}
+
+export const UploadZone: React.FC<UploadZoneProps> = (props) => {
+  return (
+    <Upload
+      directory={props.isDirectory}
+      beforeUpload={async (_, _files) => {
+        let files: FileEntry[] = [];
+        for (let file of _files) {
+          const content = await file.text();
+          console.log('files', _files);
+          files.push({ relativePath: file.webkitRelativePath || file.name, content });
+        }
+
+        props.onFiles(files);
+        return false;
+      }}
+    >
+      <div className={s.UploadZone}>
+        {props.children}
+      </div>
+    </Upload>
   );
 }
 
