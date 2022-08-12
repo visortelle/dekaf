6eaaa620f13ccd777d4ce66e7dc71417b615d3b6
visortelle <visortelle@gmail.com>
2022-08-12 18:20:33 +0200

    Correctly show message's schema version and it's JSON value

---
+++ api-scala/src/main/scala/conversions/pritimitves.scala
@@ -0,0 +1,57 @@
+package conversions
+
+import java.nio.charset.StandardCharsets
+import java.nio.{ByteBuffer, ByteOrder}
+
+def bytesToInt8(bytes: Array[Byte]): Int =
+    val buf = ByteBuffer.allocateDirect(4)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to (buf.capacity - bytes.length - 1)) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    val uint8 = buf.getInt
+    if uint8 > 127 then uint8 - 256 else uint8
+
+def bytesToInt16(bytes: Array[Byte]): Int =
+    val buf = ByteBuffer.allocateDirect(4)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to buf.capacity - bytes.length - 1) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    buf.getInt
+
+def bytesToInt32(bytes: Array[Byte]): Int =
+    val buf = ByteBuffer.allocateDirect(4)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to buf.capacity - bytes.length - 1) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    buf.getInt
+
+def bytesToInt64(bytes: Array[Byte]): Long =
+    val buf = ByteBuffer.allocateDirect(8)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to (buf.capacity - bytes.length - 1)) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    buf.getLong
+
+def bytesToFloat32(bytes: Array[Byte]): Float =
+    val buf = ByteBuffer.allocateDirect(4)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to buf.capacity - bytes.length - 1) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    buf.getFloat
+
+def bytesToFloat64(bytes: Array[Byte]): Double =
+    val buf = ByteBuffer.allocateDirect(8)
+    buf.order(ByteOrder.BIG_ENDIAN)
+    for (_ <- 0 to buf.capacity - bytes.length - 1) buf.put(0x00.toByte)
+    bytes.foreach(buf.put)
+    buf.flip
+    buf.getDouble
+
+def bytesToString(bytes: Array[Byte]): String = String(bytes, StandardCharsets.UTF_8)
+
+def bytesToBoolean(bytes: Array[Byte]): Boolean = bytes.head > 0
