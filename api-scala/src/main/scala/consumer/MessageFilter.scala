75f118599629b2ca17c426f8c7051c35ccc2c015
visortelle <visortelle@gmail.com>
2022-08-12 19:35:56 +0200

    Filters POC works

--- api-scala/src/main/scala/consumer/MessageFilter.scala
+++ api-scala/src/main/scala/consumer/MessageFilter.scala
@@ -6,19 +6,38 @@ import com.typesafe.scalalogging.Logger
 
 import org.apache.pulsar.client.api.Message
 
-case class JsonMessage()
-
 type FilterLanguage = "js" | "python"
 
 class MessageFilter():
     val logger = Logger(getClass.getName)
     val context = Context.newBuilder("js", "python").build
 
-    def test(lang: FilterLanguage, code: String, msg: Message[Array[Byte]]): Either[String, Boolean] =
-        try {
-            val value = msg.getValue()
-            logger.info(s"VALUEEEEEEEEEEEEE: ${value}")
-            Right(context.eval(lang, code).asBoolean)
-        } catch {
-            case err => Left(err.getMessage)
-        }
+    def test(lang: FilterLanguage, filterCode: String, jsonValue: String): Either[String, Boolean] =
+        testUsingJs(context, filterCodeExample, jsonValue)
+
+def testUsingJs(context: Context, filterCode: String, jsonValue: String): Either[String, Boolean] =
+    val evalCode =
+        s"""
+          | (() => {
+          |     const m = {
+          |       value: ${jsonValue},
+          |     }
+          |
+          |    return (${filterCode})(m)
+          | })()
+          |""".stripMargin
+
+    try {
+        Right(context.eval("js", evalCode).asBoolean)
+    } catch {
+        case err =>
+            println(s"UNABLE TO COMPILE ${err.getMessage}")
+            Left(err.getMessage)
+    }
+
+val filterCodeExample =
+    s"""
+      | (m) => {
+      |   return m.value.includes('a')
+      | }
+      |""".stripMargin
