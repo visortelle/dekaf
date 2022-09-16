424a38d650aea658033a8ddab688d7750b655e65
visortelle <visortelle@gmail.com>
2022-09-16 19:30:43 +0200

    Use gRPC for namespaces policies

--- pulsar-ui/components/NamespacePage/Policies/fields/anti-affinity-group.tsx
+++ pulsar-ui/components/NamespacePage/Policies/fields/anti-affinity-group.tsx
@@ -1,9 +1,11 @@
 import * as Notifications from '../../../app/contexts/Notifications';
-import * as PulsarAdminClient from '../../../app/contexts/PulsarAdminClient';
+import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
 import useSWR, { useSWRConfig } from "swr";
 import { ConfigurationField } from "../../../ui/ConfigurationTable/ConfigurationTable";
 import Input from '../../../ui/ConfigurationTable/Input/InputWithUpdateConfirmation';
 import { swrKeys } from '../../../swrKeys';
+import { DeleteNamespaceAntiAffinityGroupRequest, GetNamespaceAntiAffinityGroupRequest, SetNamespaceAntiAffinityGroupRequest } from '../../../../grpc-web/tools/teal/pulsar/ui/namespace/v1/namespace_pb';
+import { Code } from '../../../../grpc-web/google/rpc/code_pb';
 
 const policy = 'antiAffinityGroup';
 
@@ -13,16 +15,26 @@ export type FieldInputProps = {
 }
 
 export const FieldInput: React.FC<FieldInputProps> = (props) => {
-  const adminClient = PulsarAdminClient.useContext().client;
+  const { namespaceServiceClient } = PulsarGrpcClient.useContext();
   const { notifyError } = Notifications.useContext();
   const { mutate } = useSWRConfig()
 
-  const onUpdateError = (err: string) => notifyError(`Can't update anti-affinity group. ${err}`);
-  const swrKey = swrKeys.pulsar.tenants.tenant.namespaces.namespace.policies.policy({ tenant: props.tenant, namespace: props.namespace, policy  });
+  const swrKey = swrKeys.pulsar.tenants.tenant.namespaces.namespace.policies.policy({ tenant: props.tenant, namespace: props.namespace, policy });
 
   const { data: antiAffinityGroup, error: antiAffinityGroupError } = useSWR(
     swrKey,
-    async () => await adminClient.namespaces.getNamespaceAntiAffinityGroup(props.tenant, props.namespace)
+    async () => {
+      const req = new GetNamespaceAntiAffinityGroupRequest();
+      req.setNamespace(`${props.tenant}/${props.namespace}`);
+
+      const res = await namespaceServiceClient.getNamespaceAntiAffinityGroup(req, {});
+      if (res.getStatus()?.getCode() !== Code.OK) {
+        notifyError(`Can't get anti-affinity group. ${res.getStatus()?.getMessage()}`);
+        return;
+      }
+
+      return res.getNamespaceAntiAffinityGroup();
+    }
   );
 
   if (antiAffinityGroupError) {
@@ -34,16 +46,28 @@ export const FieldInput: React.FC<FieldInputProps> = (props) => {
       value={antiAffinityGroup || ''}
       onChange={async (antiAffinityGroup) => {
         if (antiAffinityGroup === '') {
-          await adminClient.namespaces.removeNamespaceAntiAffinityGroup(props.tenant, props.namespace).catch(onUpdateError);
+          const req = new DeleteNamespaceAntiAffinityGroupRequest();
+          req.setNamespace(`${props.tenant}/${props.namespace}`);
+
+          const res = await namespaceServiceClient.deleteNamespaceAntiAffinityGroup(req, {}).catch((err) => notifyError(`Can't delete anti-affinity group. ${err}`));
+          if (res?.getStatus()?.getCode() !== Code.OK) {
+            notifyError(`Can't delete anti-affinity group. ${res?.getStatus()?.getMessage()}`);
+            return;
+          }
+
           await mutate(swrKey);
           return;
         }
 
-        await adminClient.namespaces.setNamespaceAntiAffinityGroup(
-          props.tenant,
-          props.namespace,
-          antiAffinityGroup
-        ).catch(onUpdateError);
+        const req = new SetNamespaceAntiAffinityGroupRequest();
+        req.setNamespace(`${props.tenant}/${props.namespace}`);
+        req.setNamespaceAntiAffinityGroup(antiAffinityGroup);
+
+        const res = await namespaceServiceClient.setNamespaceAntiAffinityGroup(req, {}).catch((err) => notifyError(`Can't set anti-affinity group. ${err}`));
+        if (res?.getStatus()?.getCode() !== Code.OK) {
+          notifyError(`Can't set anti-affinity group. ${res?.getStatus()?.getMessage()}`);
+          return;
+        }
 
         await mutate(swrKey);
       }}
