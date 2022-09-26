41eeb274045351e5c86c39f5d4bf520b82b81f49
visortelle <visortelle@gmail.com>
2022-09-26 19:34:59 +0200

    Rewrite backlog-quota policy input

--- pulsar-ui/components/ui/ConfigurationTable/MemorySizeInput/conversions.ts
+++ pulsar-ui/components/ui/ConfigurationTable/MemorySizeInput/conversions.ts
@@ -2,15 +2,21 @@ import { MemorySize } from "./types";
 
 export function memoryToBytes(memory: MemorySize): number {
   switch (memory.unit) {
-    case 'M': return memory.size * 1024 * 1024;
-    case 'G': return memory.size * 1024 * 1024 * 1024;
+    case 'B': return memory.size;
+    case 'KB': return memory.size * 1024;
+    case 'MB': return memory.size * 1024 * 1024;
+    case 'GB': return memory.size * 1024 * 1024 * 1024;
   }
 }
 
 export function bytesToMemorySize(bytes: number): MemorySize {
-  if (bytes < 1024 * 1024 * 1024) {
-    return { size: bytes / 1024 / 1024, unit: 'M' };
+  if (bytes < 1024) {
+    return { size: bytes, unit: 'B' };
+  } else if (bytes < 1024 * 1024) {
+    return { size: bytes / 1024, unit: 'KB' };
+  } else if (bytes < 1024 * 1024 * 1024) {
+    return { size: bytes / 1024 / 1024, unit: 'MB' };
   } else {
-    return { size: bytes / 1024 / 1024 / 1024, unit: 'G' };
+    return { size: bytes / 1024 / 1024 / 1024, unit: 'GB' };
   }
 }
