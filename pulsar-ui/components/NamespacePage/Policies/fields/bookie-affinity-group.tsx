490b244154185fb340984b7923337c20cf7bdcd1
visortelle <visortelle@gmail.com>
2022-10-02 18:36:17 +0200

    Rewrite bookie-affinity-group policy input

--- pulsar-ui/components/NamespacePage/Policies/fields/bookie-affinity-group.tsx
+++ pulsar-ui/components/NamespacePage/Policies/fields/bookie-affinity-group.tsx
@@ -1,5 +1,6 @@
 import * as Notifications from '../../../app/contexts/Notifications';
-import * as PulsarAdminClient from '../../../app/contexts/PulsarAdminClient';
+import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
+import * as pb from '../../../../grpc-web/tools/teal/pulsar/ui/namespace/v1/namespace_pb';
 import useSWR, { useSWRConfig } from "swr";
 import { ConfigurationField } from "../../../ui/ConfigurationTable/ConfigurationTable";
 import Input from '../../../ui/ConfigurationTable/Input/Input';
@@ -7,6 +8,10 @@ import ListInput from '../../../ui/ConfigurationTable/ListInput/ListInput';
 import * as Either from 'fp-ts/Either';
 import sf from '../../../ui/ConfigurationTable/form.module.css';
 import { swrKeys } from '../../../swrKeys';
+import { Code } from '../../../../grpc-web/google/rpc/code_pb';
+import WithUpdateConfirmation from '../../../ui/ConfigurationTable/UpdateConfirmation/WithUpdateConfirmation';
+import Select from '../../../ui/Select/Select';
+import { uniq } from 'lodash';
 
 const policy = 'bookieAffinityGroup';
 
@@ -15,102 +20,156 @@ export type FieldInputProps = {
   namespace: string;
 }
 
+type PolicyValue = { type: 'disabled' } | {
+  type: 'specific-groups';
+  primary: string[];
+  secondary: string[];
+}
+
 export const FieldInput: React.FC<FieldInputProps> = (props) => {
-  const adminClient = PulsarAdminClient.useContext().client;
+  const { namespaceServiceClient } = PulsarGrpcClient.useContext();
   const { notifyError } = Notifications.useContext();
   const { mutate } = useSWRConfig()
 
-  const onUpdateError = (err: string) => notifyError(`Can't update bookie affinity group. ${err}`);
   const swrKey = swrKeys.pulsar.tenants.tenant.namespaces.namespace.policies.policy({ tenant: props.tenant, namespace: props.namespace, policy });
 
   const { data: bookieAffinityGroupData, error: bookieAffinityGroupError } = useSWR(
     swrKey,
-    async () => await adminClient.namespaces.getBookieAffinityGroup(props.tenant, props.namespace)
+    async () => {
+      const req = new pb.GetBookieAffinityGroupRequest();
+      req.setNamespace(`${props.tenant}/${props.namespace}`);
+
+      const res = await namespaceServiceClient.getBookieAffinityGroup(req, {});
+      if (res.getStatus()?.getCode() !== Code.OK) {
+        throw new Error(`Unable to get bookie affinity group. ${res.getStatus()?.getMessage()}`);
+      }
+
+      const bookieAffinityGroupData: PolicyValue = res.getGroupData() ? {
+        type: 'specific-groups',
+        primary: res.getGroupData()?.getPrimaryList().filter(g => g !== '') || [],
+        secondary: res.getGroupData()?.getSecondaryList().filter(g => g !== '') || [],
+      } : { type: 'disabled' };
+
+      return bookieAffinityGroupData;
+    }
   );
 
   if (bookieAffinityGroupError) {
     notifyError(`Unable to get bookie affinity group. ${bookieAffinityGroupError}`);
   }
 
-  const primaryGroups = bookieAffinityGroupData?.bookkeeperAffinityGroupPrimary?.split(',') || [];
-  const secondaryGroups = bookieAffinityGroupData?.bookkeeperAffinityGroupSecondary?.split(',') || [];
+  if (bookieAffinityGroupData === undefined) {
+    return null;
+  }
 
   return (
-    <div>
-      <div className={sf.FormItem}>
-        <strong className={sf.FormLabel}>Primary groups</strong>
-        <ListInput<string>
-          value={primaryGroups}
-          getId={(v) => v}
-          renderItem={(v) => <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{v}</span>}
-          editor={{
-            render: (v, onChange) => <Input value={v} onChange={onChange} placeholder="Enter group name" />,
-            initialValue: '',
-          }}
-          onRemove={async (roleId) => {
-            const bookkeeperAffinityGroupPrimary = primaryGroups.filter(r => r !== roleId).join(',') || undefined;
-            await adminClient.namespaces.setBookieAffinityGroup(
-              props.tenant,
-              props.namespace,
-              {
-                bookkeeperAffinityGroupPrimary,
-                bookkeeperAffinityGroupSecondary: bookieAffinityGroupData?.bookkeeperAffinityGroupSecondary
-              }).catch(onUpdateError);
-            await mutate(swrKey);
-          }}
-          onAdd={async (v) => {
-            const bookkeeperAffinityGroupPrimary = Array.from(new Set([...primaryGroups, v])).join(','); // XXX - Array.from(new Set) is a workaround for missing validation for unique groups at the Pulsar REST API side.
-            await adminClient.namespaces.setBookieAffinityGroup(
-              props.tenant,
-              props.namespace,
-              {
-                bookkeeperAffinityGroupPrimary,
-                bookkeeperAffinityGroupSecondary: bookieAffinityGroupData?.bookkeeperAffinityGroupSecondary
-              }).catch(onUpdateError);
-            await mutate(swrKey);
-          }}
-          isValid={(v) => v.length > 0 ? Either.right(undefined) : Either.left(new Error('Group name is required'))}
-        />
-      </div>
+    <WithUpdateConfirmation<PolicyValue>
+      initialValue={bookieAffinityGroupData}
+      onConfirm={async (value) => {
+        if (value.type === 'disabled') {
+          const req = new pb.DeleteBookieAffinityGroupRequest();
+          req.setNamespace(`${props.tenant}/${props.namespace}`);
+          const res = await namespaceServiceClient.deleteBookieAffinityGroup(req, {}).catch((err) => notifyError(`Unable to disable bookie affinity group policy. ${err}`));
+          if (res?.getStatus()?.getCode() !== Code.OK) {
+            notifyError(`Unable to disable bookie affinity group policy. ${res?.getStatus()?.getMessage()}`);
+          }
+        }
 
-      <div className={sf.FormItem}>
-        <strong className={sf.FormLabel}>Secondary groups</strong>
-        <ListInput<string>
-          value={secondaryGroups}
-          getId={(v) => v}
-          renderItem={(v) => <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{v}</span>}
-          editor={{
-            render: (v, onChange) => <Input value={v} onChange={onChange} placeholder="Enter group name" />,
-            initialValue: '',
-          }}
-          onRemove={async (roleId) => {
-            const bookkeeperAffinityGroupSecondary = secondaryGroups.filter(r => r !== roleId).join(',') || undefined;
-            await adminClient.namespaces.setBookieAffinityGroup(
-              props.tenant,
-              props.namespace,
-              {
-                bookkeeperAffinityGroupPrimary: bookieAffinityGroupData?.bookkeeperAffinityGroupPrimary,
-                bookkeeperAffinityGroupSecondary
-              }).catch(onUpdateError);
+        if (value.type === 'specific-groups') {
+          const req = new pb.SetBookieAffinityGroupRequest();
+          req.setNamespace(`${props.tenant}/${props.namespace}`);
+          const groupDataPb =  new pb.BookieAffinityGroupData();
+          groupDataPb.setPrimaryList(value.primary);
+          groupDataPb.setSecondaryList(value.secondary);
+          req.setGroupData(groupDataPb);
 
-            await mutate(swrKey);
-          }}
-          onAdd={async (v) => {
-            const bookkeeperAffinityGroupSecondary = Array.from(new Set([...secondaryGroups, v])).join(','); // XXX - Array.from(new Set) is a workaround for missing validation for unique groups at the Pulsar REST API side.
-            await adminClient.namespaces.setBookieAffinityGroup(
-              props.tenant,
-              props.namespace,
-              {
-                bookkeeperAffinityGroupPrimary: bookieAffinityGroupData?.bookkeeperAffinityGroupPrimary,
-                bookkeeperAffinityGroupSecondary
-              }).catch(onUpdateError);
+          const res = await namespaceServiceClient.setBookieAffinityGroup(req, {}).catch((err) => notifyError(`Unable to set bookie affinity group policy. ${err}`));
+          if (res === undefined) {
+            return;
+          }
+          if (res.getStatus()?.getCode() !== Code.OK) {
+            notifyError(`Unable to update bookie affinity group policy. ${res?.getStatus()?.getMessage()}`);
+          }
+        }
 
-            await mutate(swrKey);
-          }}
-          isValid={(v) => v.length > 0 ? Either.right(undefined) : Either.left(new Error('Group name is required'))}
-        />
-      </div>
-    </div>
+        await mutate(swrKey);
+      }}
+    >
+      {({ value, onChange }) => {
+        return (
+          <>
+            <div className={sf.FormItem}>
+              <Select<PolicyValue['type']>
+                list={[{ type: 'item', value: 'disabled', title: 'Disabled' }, { type: 'item', value: 'specific-groups', title: 'Specified groups' }]}
+                onChange={(v) => onChange(v === 'specific-groups' ? { type: 'specific-groups', primary: [], secondary: [] } : { type: 'disabled' })}
+                value={value.type}
+              />
+            </div>
+
+            {value.type === 'specific-groups' && (
+              <div className={sf.FormItem}>
+                <strong className={sf.FormLabel}>Primary groups</strong>
+
+                <ListInput<string>
+                  value={value.primary}
+                  getId={(v) => v}
+                  renderItem={(v) => <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{v}</span>}
+                  editor={{
+                    render: (v, onChange) => <Input value={v} onChange={onChange} placeholder="Enter group name" />,
+                    initialValue: '',
+                  }}
+                  onRemove={async (roleId) => {
+                    if (value.type !== 'specific-groups') {
+                      return;
+                    }
+
+                    onChange({ ...value, primary: value.primary.filter((v) => v !== roleId) });
+                  }}
+                  onAdd={async (v) => {
+                    if (value.type !== 'specific-groups') {
+                      return;
+                    }
+
+                    onChange({ ...value, primary: uniq([...value.primary, v]) });
+                  }}
+                  isValid={(v) => v.length > 0 ? Either.right(undefined) : Either.left(new Error('Group name is required'))}
+                />
+              </div>
+            )}
+
+            {value.type === 'specific-groups' && (
+              <div className={sf.FormItem}>
+                <strong className={sf.FormLabel}>Secondary groups</strong>
+                <ListInput<string>
+                  value={value.secondary}
+                  getId={(v) => v}
+                  renderItem={(v) => <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{v}</span>}
+                  editor={{
+                    render: (v, onChange) => <Input value={v} onChange={onChange} placeholder="Enter group name" />,
+                    initialValue: '',
+                  }}
+                  onRemove={async (roleId) => {
+                    if (value.type !== 'specific-groups') {
+                      return;
+                    }
+
+                    onChange({ ...value, secondary: value.secondary.filter((v) => v !== roleId) });
+                  }}
+                  onAdd={async (v) => {
+                    if (value.type !== 'specific-groups') {
+                      return;
+                    }
+
+                    onChange({ ...value, secondary: uniq([...value.secondary, v]) });
+                  }}
+                  isValid={(v) => v.length > 0 ? Either.right(undefined) : Either.left(new Error('Group name is required'))}
+                />
+              </div>
+            )}
+          </>
+        );
+      }}
+    </WithUpdateConfirmation>
   )
 }
 
