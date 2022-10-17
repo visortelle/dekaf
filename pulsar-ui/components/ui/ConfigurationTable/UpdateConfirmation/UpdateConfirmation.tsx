c2f0212a726d6c7dcc5552c947750ead8c4ff61f
visortelle <visortelle@gmail.com>
2022-10-17 12:05:48 +0200

    Rewrite persistence policy input

--- pulsar-ui/components/ui/ConfigurationTable/UpdateConfirmation/UpdateConfirmation.tsx
+++ pulsar-ui/components/ui/ConfigurationTable/UpdateConfirmation/UpdateConfirmation.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import Button from '../../Button/Button';
 import s from './UpdateConfirmation.module.css'
 
+export type ValidationError = React.ReactElement | undefined;
+
 export type UpdateConfirmationProps = {
   onReset: () => void,
   onConfirm: () => void,
+  validationError: ValidationError,
 };
 
 const UpdateConfirmation: React.FC<UpdateConfirmationProps> = (props) => {
   return (
-    <div className={s.Buttons}>
-      <Button type="regular" onClick={props.onReset} text="Reset" />
-      <Button type="primary" onClick={props.onConfirm} text="Update" />
-    </div>
+    <>
+      {props.validationError && (
+        <div className={s.ValidationError}>
+          {props.validationError}
+        </div>
+      )}
+
+      <div className={s.Buttons}>
+        <Button type="regular" onClick={props.onReset} text="Reset" />
+        <Button type="primary" onClick={props.onConfirm} text="Update" disabled={props.validationError !== undefined}/>
+      </div>
+    </>
+
   );
 }
 
