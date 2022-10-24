89f520ce97ed813464f78ba2d135be5b944477b3
visortelle <visortelle@gmail.com>
2022-10-24 18:33:44 +0200

    Rewrite subscribe rate policy input

--- pulsar-ui/components/NamespacePage/Policies/fields/subscribe-rate.tsx
+++ pulsar-ui/components/NamespacePage/Policies/fields/subscribe-rate.tsx
@@ -1,85 +1,24 @@
 import * as Notifications from '../../../app/contexts/Notifications';
-import * as PulsarAdminClient from '../../../app/contexts/PulsarAdminClient';
+import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
 import useSWR, { useSWRConfig } from "swr";
 import { ConfigurationField } from "../../../ui/ConfigurationTable/ConfigurationTable";
 import sf from '../../../ui/ConfigurationTable/form.module.css';
-import Input from "../../../ui/ConfigurationTable/Input/Input";
-import { useEffect, useState } from 'react';
-import UpdateConfirmation from '../../../ui/ConfigurationTable/UpdateConfirmation/UpdateConfirmation';
-import SelectInput from "../../../ui/ConfigurationTable/SelectInput/SelectInput";
+import Select from '../../../ui/Select/Select';
+import Input from "../../../ui/Input/Input";
 import { swrKeys } from '../../../swrKeys';
-import { isEqual } from 'lodash';
+import * as pb from '../../../../grpc-web/tools/teal/pulsar/ui/namespace/v1/namespace_pb';
+import { Code } from '../../../../grpc-web/google/rpc/code_pb';
+import WithUpdateConfirmation from '../../../ui/ConfigurationTable/UpdateConfirmation/WithUpdateConfirmation';
+import stringify from 'safe-stable-stringify';
 
 const policy = 'subscribeRate';
 
-export type SubscribeRate = 'disabled' | {
-  subscribeRate: number;
-  subscribeRatePeriod: number;
-}
-
-const defaultSubscribeRate: SubscribeRate = {
-  subscribeRate: -1,
-  subscribeRatePeriod: 30,
-}
-
-export type SubscribeRateInputProps = {
-  value: SubscribeRate;
-  onChange: (value: SubscribeRate) => void;
-};
-export const SubscribeRateInput: React.FC<SubscribeRateInputProps> = (props) => {
-  const [subscribeRate, setSubscribeRate] = useState<SubscribeRate>(props.value);
-
-  useEffect(() => {
-    setSubscribeRate(() => props.value);
-  }, [props.value]);
-
-  const showUpdateConfirmation = !isEqual(props.value, subscribeRate);
-
-  return (
-    <div>
-      <div className={sf.FormItem}>
-        <SelectInput<'enabled' | 'disabled'>
-          list={[{ type: 'item', value: 'disabled', title: 'Disabled' }, { type: 'item', value: 'enabled', title: 'Enabled' }]}
-          value={subscribeRate === 'disabled' ? 'disabled' : 'enabled'}
-          onChange={(value) => setSubscribeRate(value === 'disabled' ? 'disabled' : defaultSubscribeRate)}
-        />
-      </div>
-
-      {subscribeRate !== 'disabled' && (
-        <div>
-          <div className={sf.FormItem}>
-            <strong className={sf.FormLabel}>Subscribe rate</strong>
-            <Input
-              type='number'
-              onChange={(v) => setSubscribeRate({ ...subscribeRate, subscribeRate: Number(v) })}
-              value={String(subscribeRate.subscribeRate)}
-            />
-          </div>
-
-          <div className={sf.FormItem}>
-            <strong className={sf.FormLabel}>Subscribe rate period (sec.)</strong>
-            <Input
-              type='number'
-              onChange={(v) => setSubscribeRate({ ...subscribeRate, subscribeRatePeriod: Number(v) })}
-              value={String(subscribeRate.subscribeRatePeriod)}
-            />
-          </div>
-        </div>
-      )}
-
-      {showUpdateConfirmation && (
-        <UpdateConfirmation
-          onConfirm={() => props.onChange(subscribeRate)}
-          onReset={() => setSubscribeRate(props.value)}
-        />
-      )}
-    </div>
-  );
-}
-
-export declare type SubscribeRateData = {
-  subscribeThrottlingRatePerConsumer?: number;
-  ratePeriodInSecond?: number;
+type PolicyValue = {
+  type: 'inherited-from-broker-config'
+} | {
+  type: 'specified',
+  subscribeThrottlingRatePerConsumer: number;
+  ratePeriodInSeconds: number;
 };
 
 export type FieldInputProps = {
@@ -88,46 +27,123 @@ export type FieldInputProps = {
 }
 
 export const FieldInput: React.FC<FieldInputProps> = (props) => {
-  const adminClient = PulsarAdminClient.useContext().client;
+  const { namespaceServiceClient } = PulsarGrpcClient.useContext();
   const { notifyError } = Notifications.useContext();
   const { mutate } = useSWRConfig();
 
-  const onUpdateError = (err: string) => notifyError(`Can't update subscribe rate. ${err}`);
   const swrKey = swrKeys.pulsar.tenants.tenant.namespaces.namespace.policies.policy({ tenant: props.tenant, namespace: props.namespace, policy });
 
-  const { data: subscribeRateData, error: subscribeRateError } = useSWR(
+  const { data: initialValue, error: initialValueError } = useSWR(
     swrKey,
-    async () => await adminClient.namespaces.getSubscribeRate(props.tenant, props.namespace) as unknown as SubscribeRateData
+    async () => {
+      const req = new pb.GetSubscribeRateRequest();
+      req.setNamespace(`${props.tenant}/${props.namespace}`);
+
+      const res = await namespaceServiceClient.getSubscribeRate(req, {});
+      if (res.getStatus()?.getCode() !== Code.OK) {
+        notifyError(`Unable to get subscribe rate. ${res.getStatus()?.getMessage()}`);
+        return;
+      }
+
+      let initialValue: PolicyValue = { type: 'inherited-from-broker-config' };
+      switch (res.getSubscribeRateCase()) {
+        case pb.GetSubscribeRateResponse.SubscribeRateCase.UNSPECIFIED: {
+          initialValue = { type: 'inherited-from-broker-config' };
+          break;
+        }
+        case pb.GetSubscribeRateResponse.SubscribeRateCase.SPECIFIED: {
+          const subscribeRate = res.getSpecified()!;
+          initialValue = {
+            type: 'specified',
+            subscribeThrottlingRatePerConsumer: subscribeRate.getSubscribeThrottlingRatePerConsumer(),
+            ratePeriodInSeconds: subscribeRate.getRatePeriodInSeconds(),
+          }
+          break;
+        }
+      }
+
+      return initialValue;
+    }
   );
 
-  if (subscribeRateError) {
-    notifyError(`Unable to get subscribe rate. ${subscribeRateError}`);
+  if (initialValueError) {
+    notifyError(`Unable to get subscribe rate. ${initialValueError}`);
   }
 
-  const subscribeRate: SubscribeRate = subscribeRateData === undefined ? 'disabled' : {
-    subscribeRate: subscribeRateData.subscribeThrottlingRatePerConsumer === undefined ? defaultSubscribeRate.subscribeRate : subscribeRateData.subscribeThrottlingRatePerConsumer,
-    subscribeRatePeriod: subscribeRateData.ratePeriodInSecond === undefined ? defaultSubscribeRate.subscribeRatePeriod : subscribeRateData.ratePeriodInSecond,
+  if (initialValue === undefined) {
+    return null;
   }
 
   return (
-    <SubscribeRateInput
-      value={subscribeRate}
-      onChange={async (v) => {
-        if (v === 'disabled') {
-          await adminClient.namespaces.deleteSubscribeRate(props.tenant, props.namespace);
-        } else {
-          await adminClient.namespaces.setSubscribeRate(
-            props.tenant,
-            props.namespace,
-            {
-              subscribeThrottlingRatePerConsumer: v.subscribeRate,
-              ratePeriodInSecond: v.subscribeRatePeriod,
-            }).catch(onUpdateError);
+    <WithUpdateConfirmation<PolicyValue>
+      key={stringify(initialValue)}
+      initialValue={initialValue}
+      onConfirm={async (value) => {
+        if (value.type === 'inherited-from-broker-config') {
+          const req = new pb.RemoveSubscribeRateRequest();
+          req.setNamespace(`${props.tenant}/${props.namespace}`);
+
+          const res = await namespaceServiceClient.removeSubscribeRate(req, {});
+          if (res.getStatus()?.getCode() !== Code.OK) {
+            notifyError(`Unable to set subscribe rate. ${res.getStatus()?.getMessage()}`);
+          }
         }
 
-        await mutate(swrKey);
+        if (value.type === 'specified') {
+          const req = new pb.SetSubscribeRateRequest();
+          req.setNamespace(`${props.tenant}/${props.namespace}`);
+          req.setSubscribeThrottlingRatePerConsumer(value.subscribeThrottlingRatePerConsumer);
+          req.setRatePeriodInSeconds(value.ratePeriodInSeconds);
+
+          const res = await namespaceServiceClient.setSubscribeRate(req, {});
+          if (res.getStatus()?.getCode() !== Code.OK) {
+            notifyError(`Unable to set subscribe rate. ${res.getStatus()?.getMessage()}`);
+          }
+        }
+
+        mutate(swrKey);
       }}
-    />
+    >
+      {({ value, onChange }) => {
+        return (
+          <>
+            <div className={sf.FormItem}>
+              <Select<PolicyValue['type']>
+                list={[
+                  { type: 'item', value: 'inherited-from-broker-config', title: 'Inherited from broker config' },
+                  { type: 'item', value: 'specified', title: 'Specified' },
+                ]}
+                value={value.type}
+                onChange={(type) => {
+                  if (type === 'inherited-from-broker-config') {
+                    onChange({ type });
+                  }
+                  if (type === 'specified') {
+                    onChange({
+                      type,
+                      subscribeThrottlingRatePerConsumer: -1,
+                      ratePeriodInSeconds: 30,
+                    });
+                  }
+                }}
+              />
+            </div>
+            {value.type === 'specified' && (
+              <div>
+                <div className={sf.FormItem}>
+                  <div className={sf.FormLabel}>Rate period (sec.)</div>
+                  <Input value={String(value.ratePeriodInSeconds)} onChange={v => onChange({ ...value, ratePeriodInSeconds: Math.floor(Number(v)) })} type='number' />
+                </div>
+                <div className={sf.FormItem}>
+                  <div className={sf.FormLabel}>Subscribe throttling rate per consumer</div>
+                  <Input value={String(value.subscribeThrottlingRatePerConsumer)} onChange={v => onChange({ ...value, subscribeThrottlingRatePerConsumer: Math.floor(Number(v)) })} type='number' />
+                </div>
+              </div>
+            )}
+          </>
+        );
+      }}
+    </WithUpdateConfirmation>
   )
 }
 
