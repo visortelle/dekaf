b9106a580a9ad96b605cdbeb5c7fd58e11efe93c
visortelle <visortelle@gmail.com>
2022-11-20 16:04:27 +0100

    Serve static filex using zio-http

---
+++ server/src/main/scala/config/Config.scala
@@ -0,0 +1,54 @@
+package config
+
+import zio.*
+import zio.config.*
+import zio.config.ConfigDescriptor
+import zio.config.ConfigSource
+import zio.config.magnolia.{describe, descriptor}
+import zio.config.typesafe.{configValueConfigDescriptor, TypesafeConfig}
+import zio.config.yaml.{YamlConfig, YamlConfigSource}
+import java.nio.file.Path
+
+case class PulsarInstanceConfig(
+    @describe("Pulsar instance name")
+    name: String,
+    @describe("Optional accent color to visually distinguish this instance")
+    color: Option[String],
+    @describe("The URL where Pulsar broker (or proxy) serves protobuf requests. Example: pulsar://localhost:6650")
+    brokerServiceUrl: String,
+    @describe("The URL where Pulsar broker (or proxy) serves http requests. Example: http://localhost:8080")
+    webServiceUrl: String
+)
+
+case class Config(
+    @describe("The port HTTP server listens on")
+    httpPort: Int,
+    @describe("The port gRPC server listens on")
+    grpcPort: Int,
+    @describe("When running X-Ray behind a reverse-proxy, you need to provide a public URL to let X-Ray know how to render links and redirects correctly.")
+    publicUrl: String,
+    @describe("The Pulsar instances configuration")
+    pulsarInstances: List[PulsarInstanceConfig]
+)
+
+val defaultConfig = Config(
+  grpcPort = 8090,
+  httpPort = 8091,
+  publicUrl = "http://localhost:8091",
+  pulsarInstances = List(
+    PulsarInstanceConfig(
+      name = "local",
+      color = Some("#ff0000"),
+      brokerServiceUrl = "pulsar://localhost:6650",
+      webServiceUrl = "http://localhost:8080"
+    )
+  )
+)
+
+val configDescriptor = descriptor[Config].default(defaultConfig)
+
+val yamlConfigSource = YamlConfigSource.fromYamlPath(Path.of("./config.yaml"))
+
+val configSource = yamlConfigSource
+
+val readConfig = read(configDescriptor from configSource)
