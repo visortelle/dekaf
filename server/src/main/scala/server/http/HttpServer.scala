15284fef81f6c13aefbcaefaf2af38b9e70896f8
visortelle <visortelle@gmail.com>
2022-11-20 22:59:46 +0100

    Use freemaker templates

--- server/src/main/scala/server/http/HttpServer.scala
+++ server/src/main/scala/server/http/HttpServer.scala
@@ -4,13 +4,29 @@ import zio.*
 import zio.ZIOAppDefault
 import _root_.config.readConfig
 import io.javalin.Javalin
+import io.javalin.rendering.template.JavalinFreemarker
+
+import io.javalin.http.staticfiles.{Location, StaticFileConfig}
+import scala.jdk.CollectionConverters.*
 
 object HttpServer extends ZIOAppDefault:
-    var app = Javalin
-        .create(config => config.showJavalinBanner = false)
-        .get("/", ctx => ctx.result("Hello World"))
+    private val app = Javalin
+        .create(config => {
+            JavalinFreemarker.init()
+            config.showJavalinBanner = false
+            config.staticFiles.add((staticFiles: StaticFileConfig) => {
+                staticFiles.hostedPath = "/ui/static"
+                staticFiles.directory = "/ui/static"
+                staticFiles.location = Location.CLASSPATH
+            })
+        })
+        .get("/", ctx => {
+            val model = Map("httpPort" -> "0000").asJava
+            ctx.render("/ui/index.ftl", model)
+        })
+        .get("/health", ctx => ctx.result("OK"))
 
-    val run = for
+    val run: ZIO[Any, Throwable, Unit] = for
         config <- readConfig
         _ <- ZIO.logInfo(s"HTTP server listening on port ${config.httpPort}")
         _ <- ZIO.attempt(app.start(config.httpPort))
