a91e06a96b515a2afb03433ba152c5fca240e1e0
gibloor <gibloor@yandex.ru>
2022-11-22 12:13:52 +0100

    ready to merge

--- pulsar-ui/components/InstancePage/Configuration/ResourceGroups.tsx/ResourceGroups.tsx
+++ pulsar-ui/components/InstancePage/Configuration/ResourceGroups.tsx/ResourceGroups.tsx
@@ -1,34 +1,26 @@
-import React, { useEffect, useRef } from 'react';
-import Highlighter from "react-highlight-words";
-import ReactDOMServer from 'react-dom/server';
-import useSWR, { mutate } from 'swr';
+import React, { useState } from 'react';
+import useSWR from 'swr';
 import { useQueryParam, withDefault, StringParam, BooleanParam } from 'use-query-params';
 
-import { DynamicConfigValue } from '../Configuration';
-import ResourceGroupCreating from './ResourceGroupCreating/ResourceGroupCreating';
-import { help } from '../help';
+import ResourceGroup from './ResourceGroup/ResourceGroup';
 import { swrKeys } from '../../../swrKeys';
-import * as BrokerConfig from '../../../app/contexts/BrokersConfig';
 import * as Notifications from '../../../app/contexts/Notifications';
 import * as PulsarGrpcClient from '../../../app/contexts/PulsarGrpcClient/PulsarGrpcClient';
-import { 
-  GetResourceGroupsRequest,
-  CreateResourceGroupRequest,
-  UpdateResourceGroupRequest,
-  DeleteResourceGroupRequest,
-} from '../../../../grpc-web/tools/teal/pulsar/ui/brokers/v1/brokers_pb';
+import { GetResourceGroupsRequest } from '../../../../grpc-web/tools/teal/pulsar/ui/brokers/v1/brokers_pb';
 import { Code } from '../../../../grpc-web/google/rpc/code_pb';
 import Input from '../../../ui/Input/Input';
 import SmallButton from '../../../ui/SmallButton/SmallButton';
+import Button from '../../../ui/Button/Button';
 
 import s from './../Configuration.module.css';
 
 const ResourceGroups = () => {
 
   const { brokersServiceClient } = PulsarGrpcClient.useContext();
-  const { notifySuccess, notifyError } = Notifications.useContext();
+  const { notifyError } = Notifications.useContext();
   const [paramFilter, setParamFilter] = useQueryParam('paramFilter', withDefault(StringParam, ''));
   const [createGroup, setCreateGroup] = useQueryParam('createGroup', withDefault(BooleanParam, false));
+  const [updateGroup, setUpdateGroup] = useState(-1);
 
   const { data: availableResourceGroups, error: availableResourceGroupsError } = useSWR(
     swrKeys.pulsar.brokers.availableResourceGroups._(),
@@ -49,31 +41,15 @@ const ResourceGroups = () => {
     notifyError(`Unable to get dynamic configuration parameters list. ${availableResourceGroupsError}`);
   }
   let allGroups = availableResourceGroups;
-  // let allGroups = availableResourceGroups && availableResourceGroups[0] ? [...Object.keys(availableResourceGroups[0])] : [];
-  // let allKeys = Array.from(new Set([...Object.keys(dynamicConfig), ...Object.keys(runtimeConfig)])).sort((a, b) => a.localeCompare(b, 'en', { numeric: true }));
-
-  // if (paramFilter !== '') {
-  //   allGroups = allGroups.filter(key => key.toLowerCase().includes(paramFilter.toLowerCase()));
-  // }
-
-  const deleteResourceGroup = async (name: string) => {
-    const req = new DeleteResourceGroupRequest();
-    req.setName(name);
-    const res = await brokersServiceClient.deleteResourceGroup(req, {}).catch(err => notifyError(`Unable to delete resource group value: ${err}`));
-    if (res === undefined) {
-      return;
-    }
-    if (res.getStatus()?.getCode() !== Code.OK) {
-      notifyError(`Unable to delete resource group: ${res.getStatus()?.getMessage()}`);
-      return;
-    }
-
-    notifySuccess(`Resource group ${name} has been successfully deleted.`);
-    // setIsShowEditor(false);
-
-    await mutate(swrKeys.pulsar.brokers.availableResourceGroups._());
+  
+  if (paramFilter !== '') {
+    allGroups = allGroups?.filter(key => key.getName().toLowerCase().includes(paramFilter.toLowerCase()));
   }
 
+  const groupsSorter = () => {
+    allGroups = allGroups?.sort((a, b) => a.getName() < b.getName() ? 1 : -1)
+    console.log(allGroups)
+  }
 
   return (
     <div>
@@ -85,53 +61,51 @@ const ResourceGroups = () => {
         <div style={{ marginLeft: 'auto', marginTop: 'auto' }}>
           <SmallButton
             text={createGroup ? 'Show resource groups' : 'Create resource group'}
-            onClick={() => setCreateGroup(!createGroup)}
+            onClick={() => (setUpdateGroup(-1) , setCreateGroup(!createGroup))}
             type="primary"
           />
         </div>
       </div>
-      {createGroup && <ResourceGroupCreating /> ||
+      {createGroup && <ResourceGroup key={updateGroup} resourceGroupSettings={allGroups && allGroups[updateGroup]} /> ||
         <div className={s.ConfigurationTable}>
           <table className={s.Table}>
             <thead>
               <tr className={s.Row}>
-                <th className={s.Cell}>Group name</th>
-                <th className={s.Cell}>Publish rate in msgs</th>
+                <th className={s.Cell} onClick={() => groupsSorter()}>Group name</th>
                 <th className={s.Cell}>Publish rate in bytes</th>
-                <th className={s.Cell}>Dispatch rate in msgs</th>
+                <th className={s.Cell}>Publish rate in msgs</th>
                 <th className={s.Cell}>Dispatch rate in bytes</th>
+                <th className={s.Cell}>Dispatch rate in msgs</th>
+                <th className={s.Cell} />
               </tr>
             </thead>
             <tbody>
-              {allGroups && allGroups.map((key) => (
+              {allGroups && allGroups.map((key, index) => (
                 <tr key={key.getName()} className={s.Row}>
                   <td className={`${s.Cell} ${s.DynamicConfigCell}`}>
-                    {availableResourceGroups?.includes(key) ? (
-                      <DynamicConfigValue configKey={key.getName()} configValue={key.getName()} />
-                    ) : <span>-</span>}
+                    {key.getName()}
                   </td>
                   <td className={`${s.Cell} ${s.DynamicConfigCell}`}>
-                    {availableResourceGroups?.includes(key) ? (
-                      <DynamicConfigValue configKey={key.getDispatchRateInMsgs().toString()} configValue={key.getDispatchRateInMsgs().toString()} />
-                    ) : <span>-</span>}
+                    {key.getDispatchRateInBytes()}
                   </td>
                   <td className={`${s.Cell} ${s.DynamicConfigCell}`}>
-                    {availableResourceGroups?.includes(key) ? (
-                      <DynamicConfigValue configKey={key.getDispatchRateInBytes().toString()} configValue={key.getDispatchRateInBytes().toString()} />
-                    ) : <span>-</span>}
+                    {key.getDispatchRateInMsgs()}
                   </td>
                   <td className={`${s.Cell} ${s.DynamicConfigCell}`}>
-                    {availableResourceGroups?.includes(key) ? (
-                      <DynamicConfigValue configKey={key.getPublishRateInMsgs.toString()} configValue={key.getPublishRateInMsgs().toString()} />
-                    ) : <span>-</span>}
+                    {key.getPublishRateInBytes()}
                   </td>
                   <td className={`${s.Cell} ${s.DynamicConfigCell}`}>
-                    {availableResourceGroups?.includes(key) ? (
-                      <DynamicConfigValue configKey={key.getPublishRateInBytes().toString()} configValue={key.getPublishRateInBytes().toString()} />
-                    ) : <span>-</span>}
+                    {key.getPublishRateInMsgs()}
                   </td>
-                  <td onClick={() => deleteResourceGroup(key.getName())}>
-                    delete
+                  <td>
+                    <Button
+                      onClick={() => (setUpdateGroup(index), setCreateGroup(!createGroup))}
+                      type='primary'
+                      text='change'
+                      buttonProps={{
+                        type: 'submit'
+                      }}
+                    />
                   </td>
                 </tr>
               ))}
