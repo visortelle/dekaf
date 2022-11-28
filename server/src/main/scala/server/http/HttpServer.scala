08bb7de86f41fcd1cda8654a1cb41c98fac6105d
visortelle <visortelle@gmail.com>
2022-11-28 16:09:33 +0100

    Fix routing

--- server/src/main/scala/server/http/HttpServer.scala
+++ server/src/main/scala/server/http/HttpServer.scala
@@ -11,27 +11,30 @@ import scala.jdk.CollectionConverters.*
 
 object HttpServer extends ZIOAppDefault:
     private val app = Javalin
-        .create(config => {
+        .create { config =>
             JavalinFreemarker.init()
             config.showJavalinBanner = false
-            config.staticFiles.add((staticFiles: StaticFileConfig) => {
+            config.staticFiles.add { (staticFiles: StaticFileConfig) =>
                 staticFiles.hostedPath = "/ui/static"
                 staticFiles.directory = "/ui/static"
                 staticFiles.location = Location.CLASSPATH
-            })
-        })
-        .get("/", ctx => {
-            val model = Map(
-                "publicUrl" -> "0000"
-            ).asJava
-            ctx.render("/ui/index.ftl", model)
-        })
+            }
+            config.spaRoot.addHandler(
+              "/",
+              ctx => {
+                  val model = Map(
+                    "publicUrl" -> "0000"
+                  ).asJava
+                  ctx.render("/ui/index.ftl", model)
+              }
+            )
+        }
         .get("/health", ctx => ctx.result("OK"))
 
     val run: IO[Throwable, Unit] = for
         config <- readConfig
         port <- ZIO.attempt(config.internal.get.httpPort)
 
-        _ <- ZIO.logInfo(s"HTTP server listening on port ${port}")
+        _ <- ZIO.logInfo(s"HTTP server listening on port $port")
         _ <- ZIO.attempt(app.start(port))
     yield ()
