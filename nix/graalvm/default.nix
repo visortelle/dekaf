1f98cc63e3ea7ffffc8c9bb3e5f15f8d30144aa4
visortelle <visortelle@gmail.com>
2022-12-02 16:23:09 +0100

    Setup github actions workflow

--- nix/graalvm/default.nix
+++ nix/graalvm/default.nix
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
 
     export LD_LIBRARY_PATH="${runtimeLibraryPath}"
 
-    if (uname -a | grep "linuxkit"); then
+    if (uname -a | grep -i "linux"); then
       patchelf $out/bin/gu --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)"
       patchelf $out/bin/java --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)"
     fi
