bfce3544cad3634070a7ba938362435b5b552a08
visortelle <visortelle@gmail.com>
2022-12-03 21:01:22 +0100

    Build for arm64

--- server/src/main/scala/envoy/platform.scala
+++ server/src/main/scala/envoy/platform.scala
@@ -33,7 +33,9 @@ def getEnvoyBinResourcePath: IO[Throwable, os.ResourcePath] =
         currentArch <- getArch
         path <- (currentOs, currentArch) match
             case (Darwin(), Amd64()) => ZIO.succeed(os.resource / "envoy" / "darwin" / "amd64" / "envoy.bin")
-            case (Darwin(), Arm64()) => ZIO.succeed(os.resource / "envoy" / "darwin" / "arm64" / "envoy.bin")
+
+            // Rely on the Rosetta 2 subsystem that emulates x86_64 on Apple Silicon
+            case (Darwin(), Arm64()) => ZIO.succeed(os.resource / "envoy" / "darwin" / "amd64" / "envoy.bin")
             case _                   => ZIO.fail(new Exception(s"Unsupported OS/architecture combination: $currentOs/$currentArch"))
     yield path
 
