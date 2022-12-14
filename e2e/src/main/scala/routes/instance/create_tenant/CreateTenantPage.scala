510b1d837f43c066cd07d342a428c98e4a4ad617
Kiryl Valkovich <visortelle@gmail.com>
2022-12-14 12:57:10 +0300

    Introduce end to end tests with Playwright (#93)

---
+++ e2e/src/main/scala/routes/instance/create_tenant/CreateTenantPage.scala
@@ -0,0 +1,19 @@
+package routes.instance.create_tenant
+
+import _root_.ui.ListInput
+import com.microsoft.playwright.Locator
+import com.microsoft.playwright.Locator.{GetByPlaceholderOptions, GetByRoleOptions}
+import com.microsoft.playwright.options.AriaRole
+
+case class CreateTenantPage(root: Locator):
+    val tenantNameInput: Locator = root.getByPlaceholder("tenant-1", new GetByPlaceholderOptions().setExact(true))
+    val adminRoleInput: ListInput = ListInput(root.getByTestId("admin-roles-input"))
+    val clustersInput: ListInput = ListInput(root.getByTestId("clusters-input"))
+    val createButton: Locator = root.getByRole(AriaRole.BUTTON, new GetByRoleOptions().setName("Create").setExact(true))
+
+    def setTenantName(tenantName: String): Unit = tenantNameInput.fill(tenantName)
+    def addAdminRole(role: String): Unit = adminRoleInput.addItem(role)
+    def addCluster(cluster: String): Unit = clustersInput.addItem(cluster)
+    def create(): Unit = 
+        createButton.click()
+        root.page.waitForTimeout(1000)
---
+++ e2e/src/main/scala/ui/ListInput.scala
@@ -0,0 +1,13 @@
+package ui
+
+import com.microsoft.playwright.Locator
+import com.microsoft.playwright.Locator.GetByRoleOptions
+import com.microsoft.playwright.options.AriaRole
+
+case class ListInput(root: Locator):
+    val addItemInput: Locator = root.getByRole(AriaRole.TEXTBOX)
+    val addItemButton: Locator = root.getByRole(AriaRole.BUTTON, new GetByRoleOptions().setName("Add").setExact(true))
+
+    def addItem(text: String): Unit =
+        addItemInput.fill(text)
+        addItemButton.click()
