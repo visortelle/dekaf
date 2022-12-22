1dc46a5b53f45ce1aa8d962d67c5af013640d483
gibloor <gibloor@yandex.ru>
2022-12-22 16:25:20 +0100

    save

---
+++ e2e/src/main/scala/routes/tenants/_tenant_id/namespaces/NamespacesPage.scala
@@ -0,0 +1,21 @@
+package routes.tenants._tenant_id.namespaces
+
+import _root_.ui.ListInput
+import com.microsoft.playwright.Locator
+import com.microsoft.playwright.Locator.{GetByPlaceholderOptions, GetByRoleOptions}
+import com.microsoft.playwright.options.AriaRole
+
+case class NamespacesPage(root: Locator):
+    val saveButton: Locator = root.getByRole(AriaRole.BUTTON, new GetByRoleOptions().setName("Save").setExact(true))
+    val deleteButton: Locator = root.getByRole(AriaRole.BUTTON, new GetByRoleOptions().setName("Delete").setExact(true))
+
+    val resourceGroupName: Locator = root.getByTestId("resource-group-name")
+    val dispatchRateInBytesInput: Locator = root.getByTestId("dispatch-rate-in-bytes")
+    val dispatchRateInMsgsInput: Locator = root.getByTestId("dispatch-rate-in-msgs")
+    val publishRateInBytesInput: Locator = root.getByTestId("publish-rate-in-bytes")
+    val publishRateInMsgsInput: Locator = root.getByTestId("publish-rate-in-msgs")
+
+    def setDispatchRateInBytes(rate: String): Unit = dispatchRateInBytesInput.fill(rate)
+    def setDispatchRateInMsgs(rate: String): Unit = dispatchRateInMsgsInput.fill(rate)
+    def setPublishRateInBytes(rate: String): Unit = publishRateInBytesInput.fill(rate)
+    def setPublishRateInMsgs(rate: String): Unit = publishRateInMsgsInput.fill(rate)
---
+++ e2e/src/test/scala/routes/tenants/_tenant_id/namespaces/NamespacesPageSpec.scala
@@ -0,0 +1,39 @@
+package routes.tenants._tenant_id.namespaces
+
+import zio.*
+import zio.test.*
+import zio.test.Assertion.*
+import zio.test.TestAspect.*
+
+import scala.jdk.CollectionConverters.*
+import _root_.test_env.{TestEnv, createPulsarStandaloneEnv}
+import com.microsoft.playwright.Page.WaitForURLOptions
+import org.apache.pulsar.common.policies.data.ResourceGroup
+import net.datafaker.Faker
+
+val faker = new Faker();
+
+object NamespacesPageSpec extends ZIOSpecDefault {
+    def spec: Spec[Any, Any] = suite("Edit Resource Group page")(
+        test("User can delete tenant") {
+            val testEnv: TestEnv = createPulsarStandaloneEnv
+            val page = testEnv.createNewPage()
+            val adminClient = testEnv.createPulsarAdminClient()
+
+            val testResourceGroupName = s"${faker.name.firstName()}-${java.util.Date().getTime}"
+            adminClient.resourcegroups.createResourceGroup(testResourceGroupName, new ResourceGroup())
+            val isTestResourceGroupCreated = adminClient.resourcegroups.getResourceGroups.asScala.contains(testResourceGroupName)
+
+            page.navigate(s"/instance/resource-groups/edit/${testResourceGroupName}")
+            val editResourceGroupPage = NamespacesPage(page.locator("body"))
+
+            editResourceGroupPage.deleteButton.click()
+            page.waitForTimeout(1000)
+            val isTestResourceGroupDeleted = !adminClient.resourcegroups.getResourceGroups.asScala.contains(testResourceGroupName)
+
+            page.waitForURL("/instance/resource-groups", new WaitForURLOptions().setTimeout(3000))
+
+            assertTrue(isTestResourceGroupCreated) && assertTrue(isTestResourceGroupDeleted)
+        },
+    )
+}
