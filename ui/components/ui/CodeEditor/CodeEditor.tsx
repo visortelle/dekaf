c1c003884c9332e4fafdebdb4f41bdc7a6caf7e0
gibloor <gibloor@yandex.ru>
2022-12-27 14:26:25 +0100

    save

--- ui/components/ui/CodeEditor/CodeEditor.tsx
+++ ui/components/ui/CodeEditor/CodeEditor.tsx
@@ -8,9 +8,43 @@ export type CodeEditorProps = EditorProps;
 const CodeEditor: React.FC<CodeEditorProps> = (props) => {
   const { options, ...restProps } = props;
 
+  const onMount = (monaco: any) => {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [
+        {
+          uri: "http://myserver/foo-schema.json",
+          schema: {
+            type: "object",
+            properties: {
+              p1: {
+                enum: ["v1", "v2"],
+              },
+              p2: {
+                $ref: "http://myserver/bar-schema.json",
+              },
+            },
+          },
+        },
+        {
+          uri: "http://myserver/bar-schema.json",
+          schema: {
+            type: "object",
+            properties: {
+              q1: {
+                enum: ["x1", "x2"],
+              },
+            },
+          },
+        },
+      ],
+    });
+  };
+
   return (
     <div className={s.CodeEditor}>
       <Editor
+        onMount={onMount}
         options={{
           minimap: { enabled: false },
           scrollbar: { alwaysConsumeMouseWheel: false, useShadows: false },
