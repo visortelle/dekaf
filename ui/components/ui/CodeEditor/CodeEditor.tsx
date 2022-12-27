e42b75a727221811c58d6253d4c95192bf797047
gibloor <gibloor@yandex.ru>
2022-12-27 15:21:37 +0100

    save

--- ui/components/ui/CodeEditor/CodeEditor.tsx
+++ ui/components/ui/CodeEditor/CodeEditor.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Editor, { EditorProps } from "@monaco-editor/react";
+import loader from '@monaco-editor/loader';
 
 import s from './CodeEditor.module.css'
 
@@ -8,43 +9,78 @@ export type CodeEditorProps = EditorProps;
 const CodeEditor: React.FC<CodeEditorProps> = (props) => {
   const { options, ...restProps } = props;
 
-  const onMount = (monaco: any) => {
-    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-      validate: true,
-      schemas: [
-        {
-          uri: "http://myserver/foo-schema.json",
-          schema: {
-            type: "object",
-            properties: {
-              p1: {
-                enum: ["v1", "v2"],
-              },
-              p2: {
-                $ref: "http://myserver/bar-schema.json",
-              },
-            },
-          },
+  useEffect(() => {
+    loader.init().then((monaco) => {
+      // Register a new language
+      monaco.languages.register({ id: 'mySpecialLanguage' });
+
+      // Register a tokens provider for the language
+      monaco.languages.setMonarchTokensProvider('mySpecialLanguage', {
+        tokenizer: {
+          root: [
+            [/\[error.*/, 'custom-error'],
+            [/\[notice.*/, 'custom-notice'],
+            [/\[info.*/, 'custom-info'],
+            [/\[[a-zA-Z 0-9:]+\]/, 'custom-date'],
+          ],
         },
-        {
-          uri: "http://myserver/bar-schema.json",
-          schema: {
-            type: "object",
-            properties: {
-              q1: {
-                enum: ["x1", "x2"],
-              },
-            },
-          },
+      });
+
+      // Define a new theme that contains only rules that match this language
+      monaco.editor.defineTheme('myCoolTheme', {
+        base: 'vs',
+        inherit: false,
+        rules: [
+          { token: 'custom-info', foreground: '808080' },
+          { token: 'custom-error', foreground: 'FF0000', fontStyle: 'bold' },
+          { token: 'custom-notice', foreground: 'FFA500' },
+          { token: 'custom-date', foreground: '008800' },
+        ],
+        colors: {
+          'editor.foreground': '#000000',
         },
-      ],
-    });
-  };
+      });
+
+      // Register a completion item provider for the new language
+      monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+        provideCompletionItems: () => {
+          var suggestions = [
+            {
+              label: 'simpleText',
+              kind: monaco.languages.CompletionItemKind.Text,
+              insertText: 'simpleText',
+            },
+            {
+              label: 'testing',
+              kind: monaco.languages.CompletionItemKind.Keyword,
+              insertText: 'testing(${1:condition})',
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            },
+            {
+              label: 'ifelse',
+              kind: monaco.languages.CompletionItemKind.Snippet,
+              insertText: [
+                'if (${1:condition}) {',
+                '\t$0',
+                '} else {',
+                '\t',
+                '}',
+              ].join('\n'),
+              insertTextRules:
+                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+              documentation: 'If-Else Statement',
+            },
+          ];
+          return { suggestions: suggestions };
+        },
+      })
+    })
+  }, [])
 
   return (
     <div className={s.CodeEditor}>
       <Editor
-        onMount={onMount}
         options={{
           minimap: { enabled: false },
           scrollbar: { alwaysConsumeMouseWheel: false, useShadows: false },
