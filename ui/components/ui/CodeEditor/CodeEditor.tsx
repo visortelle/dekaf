3836920882419db6efa767a2920fdf317880ca39
gibloor <gibloor@yandex.ru>
2022-12-28 14:09:38 +0100

    save

--- ui/components/ui/CodeEditor/CodeEditor.tsx
+++ ui/components/ui/CodeEditor/CodeEditor.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
-import Editor, { EditorProps } from "@monaco-editor/react";
+import Editor, { EditorProps, Monaco, OnMount } from "@monaco-editor/react";
 import loader from '@monaco-editor/loader';
+import { IRange,  } from 'monaco-editor';
+// import { IStandaloneCodeEditor } from 'monaco-editor/esm/vs/editor/editor.api'
+
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
 
 import s from './CodeEditor.module.css'
 
@@ -9,85 +14,229 @@ export type CodeEditorProps = EditorProps;
 const CodeEditor: React.FC<CodeEditorProps> = (props) => {
   const { options, ...restProps } = props;
 
-  useEffect(() => {
-    loader.init().then((monaco) => {
-      // Register a new language
-      monaco.languages.register({ id: 'mySpecialLanguage' });
+  const editorWillMount = (monaco: Monaco, editor: monaco.editor.IStandaloneCodeEditor) => {
 
-      // Register a tokens provider for the language
-      monaco.languages.setMonarchTokensProvider('mySpecialLanguage', {
-        tokenizer: {
-          root: [
-            [/\[error.*/, 'custom-error'],
-            [/\[notice.*/, 'custom-notice'],
-            [/\[info.*/, 'custom-info'],
-            [/\[[a-zA-Z 0-9:]+\]/, 'custom-date'],
-          ],
+    const createDependencyProposals = (range: IRange) => {
+      return [
+        {
+          label: 'properties',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'properties: [key -> value]',
+          range: range
         },
-      });
+        {
+          label: 'eventTime',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'eventTime: 10',
+          range: range
+        },
+        {
+          label: 'publishTime',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'publishTime: 10',
+          range: range
+        },
+        {
+          label: 'brokerPublishTime',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'brokerPublishTime: 10',
+          range: range
+        },
+        {
+          label: 'messageId',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'messageId: [10, 20, 30]',
+          range: range
+        },
+        {
+          label: 'sequenceId',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'sequenceId: 10',
+          range: range
+        },
+        {
+          label: 'producerName',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'producerName: producer-1',
+          range: range
+        },
+        {
+          label: 'key',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'key: key-1',
+          range: range
+        },
+        {
+          label: 'orderingKey',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'orderingKey: [10, 20, 30]',
+          range: range
+        },
+        {
+          label: 'topic',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'topic: topic-1',
+          range: range
+        },
+        {
+          label: 'size',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'size: 10',
+          range: range
+        },
+        {
+          label: 'redeliveryCount',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'redeliveryCount: 10',
+          range: range
+        },
+        {
+          label: 'schemaVersion',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'schemaVersion: 10',
+          range: range
+        },
+        {
+          label: 'isReplicated',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'isReplicated: true',
+          range: range
+        },
+        {
+          label: 'replicatedFrom',
+          kind: monaco.languages.CompletionItemKind.Function,
+          documentation: 'More about ...',
+          insertText: 'replicatedFrom: replicatedFrom-1',
+          range: range
+        },
+      ];
+    }
+    
+    // monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+    //   allowJs: true,
+    //   allowSyntheticDefaultImports: true,
+    //   allowUmdGlobalAccess: true,
+    //   allowUnreachableCode: true,
+    //   allowUnusedLabels: true,
 
-      // Define a new theme that contains only rules that match this language
-      monaco.editor.defineTheme('myCoolTheme', {
-        base: 'vs',
-        inherit: false,
-        rules: [
-          { token: 'custom-info', foreground: '808080' },
-          { token: 'custom-error', foreground: 'FF0000', fontStyle: 'bold' },
-          { token: 'custom-notice', foreground: 'FFA500' },
-          { token: 'custom-date', foreground: '008800' },
-        ],
-        colors: {
-          'editor.foreground': '#000000',
-        },
-      });
+    //   alwaysStrict: true,
+    //   checkJs: true,
+    //   declaration: true,
+    //   declarationMap: true,
+    //   emitDeclarationOnly: true,
+    //   disableSizeLimit: true,
+    //   disableSourceOfProjectReferenceRedirect: true,
+    //   downlevelIteration: true,
+    //   emitBOM: true,
+    //   emitDecoratorMetadata: true,
+    //   experimentalDecorators: true,
+    //   forceConsistentCasingInFileNames: true,
+    //   importHelpers: true,
+    //   inlineSourceMap: true,
+    //   inlineSources: true,
+    //   isolatedModules: true,
+    //   keyofStringsOnly: true,
+    //   preserveConstEnums: true,
+    //   preserveSymlinks: true,
+    //   composite: true,
+    //   removeComments: true,
+    //   skipLibCheck: true,
+    //   skipDefaultLibCheck: true,
+    //   sourceMap: true,
+    //   strict: true,
+    //   strictFunctionTypes: true,
+    //   strictBindCallApply: true,
+    //   strictNullChecks: true,
+    //   strictPropertyInitialization: true,
+    //   stripInternal: true,
+    //   suppressExcessPropertyErrors: true,
+    //   suppressImplicitAnyIndexErrors: true,
+    //   traceResolution: true,
+    //   resolveJsonModule: true,
+    //   esModuleInterop: true,
+    //   useDefineForClassFields: true,
 
-      // Register a completion item provider for the new language
-      monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
-        provideCompletionItems: () => {
-          var suggestions = [
-            {
-              label: 'simpleText',
-              kind: monaco.languages.CompletionItemKind.Text,
-              insertText: 'simpleText',
-            },
-            {
-              label: 'testing',
-              kind: monaco.languages.CompletionItemKind.Keyword,
-              insertText: 'testing(${1:condition})',
-              insertTextRules:
-                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-            },
-            {
-              label: 'ifelse',
-              kind: monaco.languages.CompletionItemKind.Snippet,
-              insertText: [
-                'if (${1:condition}) {',
-                '\t$0',
-                '} else {',
-                '\t',
-                '}',
-              ].join('\n'),
-              insertTextRules:
-                monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-              documentation: 'If-Else Statement',
-            },
-          ];
-          return { suggestions: suggestions };
-        },
-      })
-    })
-  }, [])
+    //   noEmit: false,
+    //   noEmitHelpers: false,
+    //   noEmitOnError: false,
+    //   noErrorTruncation: false,
+    //   noFallthroughCasesInSwitch: false,
+    //   noImplicitAny: false,
+    //   noImplicitReturns: false,
+    //   noImplicitThis: false,
+    //   noStrictGenericChecks: false,
+    //   noUnusedLocals: false,
+    //   noUnusedParameters: false,
+    //   noImplicitUseStrict: false,
+    //   noLib: false,
+    //   noResolve: false,
+    // })
+
+    // monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+    //   noSemanticValidation: false,
+    //   noSyntaxValidation: false,
+    // });
+
+    monaco.languages.registerCompletionItemProvider('javascript', {
+      provideCompletionItems: function (model, position) {
+    
+        const textUntilPosition = model.getValueInRange({
+          startLineNumber: 1,
+          startColumn: 1,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column
+        })
+        const match = textUntilPosition.match(
+          // /"dependencies"\s*:\s*\{\s*("[^"]*"\s*:\s*"[^"]*"\s*,\s*)*([^"]*)?$/
+          // /msg.*/
+          /msg.*?$/
+        )
+        if (!match) {
+          return { suggestions: [] };
+        }
+        const word = model.getWordUntilPosition(position);
+        const range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,
+          endColumn: word.endColumn
+        }
+        return {
+          suggestions: createDependencyProposals(range)
+        }
+      }
+    });
+  }
+  
 
   return (
     <div className={s.CodeEditor}>
       <Editor
+        onMount={(editor, monaco) => editorWillMount(monaco, editor)}
         options={{
           minimap: { enabled: false },
           scrollbar: { alwaysConsumeMouseWheel: false, useShadows: false },
           theme: 'vs',
           fontFamily: 'Fira Code',
           fontSize: parseFloat(getComputedStyle(document.documentElement).fontSize) * 14,
-          ...options
+          ...options,
+          suggest: {
+            showFunctions: false
+          }
         }}
         {...restProps}
       />
