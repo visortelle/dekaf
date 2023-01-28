6a075ef3c77aeaeea30c86678a99e160dffcdbc9
visortelle <visortelle@gmail.com>
2023-01-28 15:05:25 +0100

    More complex message type for PROTOBUF_NATIVE converters test

--- server/src/test/scala/schema/protobufnative/convertersTest.scala
+++ server/src/test/scala/schema/protobufnative/convertersTest.scala
@@ -14,12 +14,19 @@ object convertersTest extends ZIOSpecDefault {
             val protoFileContent = """
                 |syntax = "proto3";
                 |
+                |message Occupation {
+                |  string person_company = 1;
+                |  string person_position = 2;
+                |  int32 person_salary = 3;
+                |}
+                |
                 |message Person {
                 |  string name = 1;
                 |  int32 age = 2;
                 |
                 |  // https://github.com/protocolbuffers/protobuf/issues/5015
                 |  int64 height = 3;
+                |  Occupation person_occupation = 4;
                 |}
              """.stripMargin
 
@@ -28,17 +35,18 @@ object convertersTest extends ZIOSpecDefault {
             val compiledFiles = compiler.compileFiles(Seq(fileEntry))
 
             val file = compiledFiles.files.getOrElse(relativePath, Left("No such file")) match
-                case Right(f) => f
+                case Right(f)             => f
                 case Left(err: Throwable) => throw err
 
             val rawSchema = file.schemas.get("Person") match
                 case Some(schema) => schema.rawSchema
                 case None         => throw new Exception("No such schema")
 
-            val jsonInput = """{"name":"John","age":30,"height":"180"}"""
+            val jsonInput =
+                """{"name":"John","age":30,"height":"180","person_occupation":{"person_company":"Google","person_position":"Software Engineer","person_salary":100}}"""
 
             val protoResult = converters.fromJson(rawSchema, jsonInput.getBytes) match
-                case Left(err) => throw new Exception(err)
+                case Left(err)     => throw new Exception(err)
                 case Right(result) => result
 
             val reverseConversionResult = converters.toJson(rawSchema, protoResult) match
