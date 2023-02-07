a9aa3deb5684694c75195a42fdcc85802517f0fa
visortelle <visortelle@gmail.com>
2023-02-07 10:34:30 +0100

    Update favicon and page title on resource change

--- ui/components/ui/BreadCrumbs/BreadCrumbs.tsx
+++ ui/components/ui/BreadCrumbs/BreadCrumbs.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import s from './BreadCrumbs.module.css'
-import { TenantIcon, NamespaceIcon, TopicIcon, InstanceIcon } from '../Icons/Icons';
-import * as Notifications from '../../app/contexts/Notifications';
-import SvgIcon from '../SvgIcon/SvgIcon';
-import arrowIcon from './arrow.svg';
-import copyIcon from './copy.svg';
-import Link from '../Link/Link';
-import { routes } from '../../routes';
-import { mutate } from 'swr';
-import { swrKeys } from '../../swrKeys';
-import Button from '../Button/Button';
+import React from "react";
+import s from "./BreadCrumbs.module.css";
+import { TenantIcon, NamespaceIcon, TopicIcon, InstanceIcon } from "../Icons/Icons";
+import * as Notifications from "../../app/contexts/Notifications";
+import SvgIcon from "../SvgIcon/SvgIcon";
+import arrowIcon from "./arrow.svg";
+import copyIcon from "./copy.svg";
+import Link from "../Link/Link";
+import { routes } from "../../routes";
+import { mutate } from "swr";
+import { swrKeys } from "../../swrKeys";
+import Button from "../Button/Button";
+import Favicons from "./favicons/Favicons";
 
-export type CrumbType =
-  'instance' |
-  'tenant' |
-  'namespace' |
-  'persistent-topic' |
-  'non-persistent-topic' |
-  'link';
+export type CrumbType = "instance" | "tenant" | "namespace" | "persistent-topic" | "non-persistent-topic" | "link";
 
 export type Crumb = {
   id: string;
   type: CrumbType;
-  value: string
+  value: string;
 };
 
 export type BreadCrumbsProps = {
@@ -38,68 +33,112 @@ const BreadCrumbs: React.FC<BreadCrumbsProps> = (props) => {
   const renderCrumb = (crumb: Crumb, i: number, total: number) => {
     let icon = null;
     switch (crumb.type) {
-      case 'instance': icon = <InstanceIcon />; break;
-      case 'tenant': icon = <TenantIcon />; break;
-      case 'namespace': icon = <NamespaceIcon />; break;
-      case 'persistent-topic': icon = <TopicIcon topicType='persistent' />; break;
-      case 'non-persistent-topic': icon = <TopicIcon topicType='non-persistent' />; break;
+      case "instance":
+        icon = <InstanceIcon />;
+        break;
+      case "tenant":
+        icon = <TenantIcon />;
+        break;
+      case "namespace":
+        icon = <NamespaceIcon />;
+        break;
+      case "persistent-topic":
+        icon = <TopicIcon topicType='persistent' />;
+        break;
+      case "non-persistent-topic":
+        icon = <TopicIcon topicType='non-persistent' />;
+        break;
     }
 
     const isLast = i === total - 1;
 
-    let href = '#';
+    let href = "#";
     switch (crumb.type) {
-      case 'instance': href = routes.instance.tenants._.get(); break;
-      case 'tenant': href = routes.tenants.tenant.namespaces._.get({ tenant }); break;
-      case 'namespace': href = routes.tenants.tenant.namespaces.namespace.topics._.get({ tenant, namespace }); break;
-      case 'persistent-topic': href = routes.tenants.tenant.namespaces.namespace.topics.anyTopicType.topic.messages._.get({ tenant, namespace, topic, topicType: 'persistent' }); break;
-      case 'non-persistent-topic': href = routes.tenants.tenant.namespaces.namespace.topics.anyTopicType.topic.messages._.get({ tenant, namespace, topic, topicType: 'non-persistent' }); break;
-      case 'link': href = crumb.value; break;
+      case "instance":
+        href = routes.instance.tenants._.get();
+        break;
+      case "tenant":
+        href = routes.tenants.tenant.namespaces._.get({ tenant });
+        break;
+      case "namespace":
+        href = routes.tenants.tenant.namespaces.namespace.topics._.get({ tenant, namespace });
+        break;
+      case "persistent-topic":
+        href = routes.tenants.tenant.namespaces.namespace.topics.anyTopicType.topic.messages._.get({
+          tenant,
+          namespace,
+          topic,
+          topicType: "persistent",
+        });
+        break;
+      case "non-persistent-topic":
+        href = routes.tenants.tenant.namespaces.namespace.topics.anyTopicType.topic.messages._.get({
+          tenant,
+          namespace,
+          topic,
+          topicType: "non-persistent",
+        });
+        break;
+      case "link":
+        href = crumb.value;
+        break;
     }
 
     const onClick = () => {
       switch (crumb.type) {
-        case 'instance': mutate(swrKeys.pulsar.tenants._()); break;
-        case 'tenant': mutate(swrKeys.pulsar.tenants.tenant.namespaces._({ tenant })); break;
-        case 'namespace': {
+        case "instance":
+          mutate(swrKeys.pulsar.tenants._());
+          break;
+        case "tenant":
+          mutate(swrKeys.pulsar.tenants.tenant.namespaces._({ tenant }));
+          break;
+        case "namespace": {
           mutate(swrKeys.pulsar.tenants.tenant.namespaces.namespace.persistentTopics._({ tenant, namespace }));
           mutate(swrKeys.pulsar.tenants.tenant.namespaces.namespace.nonPersistentTopics._({ tenant, namespace }));
-        }; break;
+        }
+        break;
       }
-    }
+    };
 
     return (
       <Link key={crumb.id} className={s.Crumb} to={href} onClick={onClick}>
         <div className={s.CrumbIcon}>{icon}</div>
         <div className={s.CrumbTitle}>{crumb.value}</div>
-        {!isLast && <div className={s.CrumbArrow}><SvgIcon svg={arrowIcon} /></div>}
+        {!isLast && (
+          <div className={s.CrumbArrow}>
+            <SvgIcon svg={arrowIcon} />
+          </div>
+        )}
       </Link>
     );
-  }
+  };
 
   const qualifiedResourceName = crumbsToQualifiedName(props.crumbs);
   return (
-    <div className={s.BreadCrumbs}>
-      {props.crumbs.map((crumb, i) => renderCrumb(crumb, i, props.crumbs.length))}
+    <>
+      <Favicons crumbs={props.crumbs} />
+      <div className={s.BreadCrumbs}>
+        {props.crumbs.map((crumb, i) => renderCrumb(crumb, i, props.crumbs.length))}
 
-      {qualifiedResourceName !== undefined && (
-        <div className={s.CopyNameButton}>
-          <Button
-            onClick={() => {
-              if (qualifiedResourceName !== undefined) {
-                navigator.clipboard.writeText(qualifiedResourceName);
-                notifySuccess(<div>Fully qualified resource name copied to clipboard: {qualifiedResourceName}</div>);
-              }
-            }}
-            svgIcon={copyIcon}
-            type={'regular'}
-            title="Copy path to clipboard"
-          />
-        </div>
-      )}
-    </div>
+        {qualifiedResourceName !== undefined && (
+          <div className={s.CopyNameButton}>
+            <Button
+              onClick={() => {
+                if (qualifiedResourceName !== undefined) {
+                  navigator.clipboard.writeText(qualifiedResourceName);
+                  notifySuccess(<div>Fully qualified resource name copied to clipboard: {qualifiedResourceName}</div>);
+                }
+              }}
+              svgIcon={copyIcon}
+              type={"regular"}
+              title="Copy path to clipboard"
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
-}
+};
 
 function crumbsToQualifiedName(crumbs: Crumb[]): string | undefined {
   const tenant = crumbs[1]?.value;
@@ -107,16 +146,20 @@ function crumbsToQualifiedName(crumbs: Crumb[]): string | undefined {
   const topic = crumbs[3]?.value;
 
   switch (crumbs[crumbs.length - 1].type) {
-    case 'tenant': return tenant;
-    case 'namespace': return `${tenant}/${namespace}`;
-    case 'persistent-topic': return `${'persistent'}://${tenant}/${namespace}/${topic}`;
-    case 'non-persistent-topic': return `${'non-persistent'}://${tenant}/${namespace}/${topic}`;
-    default: return undefined;
+    case "tenant":
+      return tenant;
+    case "namespace":
+      return `${tenant}/${namespace}`;
+    case "persistent-topic":
+      return `${"persistent"}://${tenant}/${namespace}/${topic}`;
+    case "non-persistent-topic":
+      return `${"non-persistent"}://${tenant}/${namespace}/${topic}`;
+    default:
+      return undefined;
   }
 }
 
 export default BreadCrumbs;
-
 
 export const BreadCrumbsAtPageTop: React.FC<BreadCrumbsProps> = (props) => {
   return (
@@ -124,4 +167,4 @@ export const BreadCrumbsAtPageTop: React.FC<BreadCrumbsProps> = (props) => {
       <BreadCrumbs {...props} />
     </div>
   );
-}
+};
