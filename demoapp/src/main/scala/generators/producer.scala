26bf84cd7b4967a431ee9a4758caefec35d9a4ff
visortelle <visortelle@gmail.com>
2023-07-05 11:33:41 +0200

    Demoapp - schemas/bool

--- demoapp/src/main/scala/generators/producer.scala
+++ demoapp/src/main/scala/generators/producer.scala
@@ -8,27 +8,29 @@ type ProducerName = String
 case class ProducerPlan(
     name: ProducerName,
     schedule: Schedule[Any, Any, Any],
-    getPayload: ProducerIndex => Array[Byte]
+    getPayload: MessageIndex => Array[Byte],
+    var messageIndex: MessageIndex
 )
 
 object ProducerPlan:
     def make(generator: ProducerPlanGenerator, producerIndex: ProducerIndex): ProducerPlan =
         ProducerPlan(
             name = generator.getName(producerIndex),
-            getPayload = _ => generator.getPayload(producerIndex),
+            getPayload = generator.getPayload(producerIndex),
             schedule = generator.getSchedule(producerIndex),
+            messageIndex = 0
         )
 
 case class ProducerPlanGenerator(
     getName: ProducerIndex => String,
-    getPayload: ProducerIndex => Array[Byte] = _ => Array.emptyByteArray,
+    getPayload: ProducerIndex => MessageIndex => Array[Byte],
     getSchedule: ProducerIndex => Schedule[Any, Any, Any]
 )
 
 object ProducerPlanGenerator:
     def make(
         getName: ProducerIndex => String = producerIndex => s"producer-$producerIndex",
-        getPayload: ProducerIndex => Array[Byte] = _ => Array.emptyByteArray,
+        getPayload: ProducerIndex => MessageIndex => Array[Byte] = _ => _ => Array.emptyByteArray,
         getSchedule: ProducerIndex => Schedule[Any, Any, Any] = _ => Schedule.fixed(Duration.fromSeconds(1))
     ): ProducerPlanGenerator =
         ProducerPlanGenerator(
