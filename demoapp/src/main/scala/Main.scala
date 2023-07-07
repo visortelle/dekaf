0aa732f5abfc79f516fd10c74cfb347cffc95338
visortelle <visortelle@gmail.com>
2023-07-07 18:56:39 +0200

    Demoapp - tweaks

--- demoapp/src/main/scala/Main.scala
+++ demoapp/src/main/scala/Main.scala
@@ -1,14 +1,20 @@
 import zio.*
-import _root_.generators
-import _root_.generators.{NamespaceIndex, NamespacePlan, NamespacePlanExecutor, NamespacePlanGenerator, ProducerPlanGenerator, TenantPlan, TenantPlanExecutor, TenantPlanGenerator, TopicPlan, TopicPlanExecutor, TopicPlanGenerator}
+import generators.*
 import com.fasterxml.jackson.annotation.PropertyAccessor
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.avro.AvroMapper
 import com.fasterxml.jackson.dataformat.protobuf.schema.ProtobufSchemaLoader
 import com.google.protobuf.GeneratedMessageV3
 import demo.schemas.SchemasTenant
-import org.apache.pulsar.client.impl.schema.{AvroSchema, JSONSchema, ProtobufNativeSchema, ProtobufNativeSchemaUtils, ProtobufSchema}
+import org.apache.pulsar.client.impl.schema.{
+    AvroSchema,
+    JSONSchema,
+    ProtobufNativeSchema,
+    ProtobufNativeSchemaUtils,
+    ProtobufSchema
+}
 import org.apache.pulsar.common.schema.{SchemaInfo, SchemaType}
+import client.{adminClient, pulsarClient}
 
 import scala.jdk.CollectionConverters.*
 import org.apache.pulsar.client.api.schema.SchemaDefinition
@@ -32,25 +38,6 @@ case class Person(
 )
 given personJsonEncoder: Encoder[Person] = deriveEncoder[Person]
 given personJsonDecoder: Decoder[Person] = deriveDecoder[Person]
-
-object Encoders:
-    import org.apache.avro.Schema
-    import com.fasterxml.jackson.dataformat.avro.{AvroFactory, AvroMapper}
-    import com.fasterxml.jackson.dataformat.protobuf.ProtobufMapper
-    import com.fasterxml.jackson.annotation.PropertyAccessor
-    import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility
-
-    def toJson[T](using encoder: io.circe.Encoder[T])(value: T): Array[Byte] =
-        value.asJson.toString.getBytes("UTF-8")
-
-    def toAvro[T](schema: Array[Byte], value: T): Array[Byte] =
-        // https://github.com/FasterXML/jackson-dataformats-binary/tree/2.16/avro
-        val mapper = new AvroMapper()
-        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
-        val avroSchema = mapper.schemaFrom(schema.map(_.toChar).mkString) // TODO: cache schema
-        mapper.writer(avroSchema).writeValueAsBytes(value)
-
-    def toProto[T <: GeneratedMessageV3](value: T): Array[Byte] = value.toByteArray
 
 val personSchemaInfoProtobuf = ProtobufSchema
     .of(classOf[com.tools.teal.pulsar.ui.test.delivery_app.v1.Person])
@@ -91,19 +78,21 @@ def mkTenantPlanGenerator = {
                         mkProducersCount = _ => 3,
                         mkProducerGenerator = _ =>
                             ProducerPlanGenerator.make(
-                                mkSchedule = _ => Schedule.fixed(Duration.fromMillis(1000)),
-                                mkPayload = _ => _ =>
-                                    val v = com.tools.teal.pulsar.ui.test.delivery_app.v1.Person
-                                        .newBuilder()
-                                        .setName(faker.funnyName.name())
-                                        .setAge(faker.number.numberBetween(0, 120))
-                                        .addAllHobbies(() =>
-                                            List.tabulate(faker.number.numberBetween(0, 10))(_ => faker.hobby.activity)
-                                                .asJava
-                                                .iterator()
-                                        )
-                                        .build()
-                                    Encoders.toProto(v)
+                                mkSchedule = _ => Schedule.fixed(Duration.fromMillis(100)),
+                                mkPayload = _ =>
+                                    _ =>
+                                        val v = com.tools.teal.pulsar.ui.test.delivery_app.v1.Person
+                                            .newBuilder()
+                                            .setName(faker.funnyName.name())
+                                            .setAge(faker.number.numberBetween(0, 120))
+                                            .addAllHobbies(() =>
+                                                List.tabulate(faker.number.numberBetween(0, 10))(_ =>
+                                                    faker.hobby.activity
+                                                ).asJava
+                                                    .iterator()
+                                            )
+                                            .build()
+                                        Encoders.toProto(v)
                             ),
                         mkSubscriptionsCount = _ => 3,
                         mkSchemaInfos = _ =>
@@ -115,15 +104,25 @@ def mkTenantPlanGenerator = {
 }
 
 object PulsocatDemoApp extends ZIOAppDefault:
-    def run = for {
+    private def appLogic = for {
         schemasTenantPlan <- SchemasTenant.mkTenantPlan
-        
-        tenantPlanGenerator <- mkTenantPlanGenerator
-        tenantPlan <- TenantPlan.make(tenantPlanGenerator, 0)
 
-        tenantPlans = List(schemasTenantPlan, tenantPlan)
+//        tenantPlanGenerator <- mkTenantPlanGenerator
+//        tenantPlan <- TenantPlan.make(tenantPlanGenerator, 0)
+
+        tenantPlans = List(
+            schemasTenantPlan
+//          tenantPlan
+        )
 
         _ <- ZIO.logInfo("Starting app...")
         _ <- ZIO.foreachParDiscard(tenantPlans)(tenantPlan => TenantPlanExecutor.allocateResources(tenantPlan))
         _ <- ZIO.foreachParDiscard(tenantPlans)(tenantPlan => TenantPlanExecutor.start(tenantPlan))
     } yield ()
+
+    private def cleanup = for {
+        _ <- ZIO.attempt(adminClient.close())
+        _ <- ZIO.attempt(pulsarClient.close())
+    } yield ()
+
+    override def run = appLogic.onExit(_ => ZIO.succeedBlocking(cleanup))
