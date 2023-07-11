9ede36d8adee4b23ae60309ecd811369ce0afe03
Alexey Samovich <47789499+alexesom@users.noreply.github.com>
2023-07-11 10:14:23 +0200

    Fix broken auth in firefox safari (#151)
     #145 Fixed auth bug in firefox and safari and fixed auth flow.

--- ui/components/app/pulsar-auth/Button/Button.tsx
+++ ui/components/app/pulsar-auth/Button/Button.tsx
@@ -7,7 +7,7 @@ import { swrKeys } from '../../../swrKeys';
 import { GetCurrentCredentialsRequest } from '../../../../grpc-web/tools/teal/pulsar/ui/api/v1/pulsar_auth_pb';
 import * as GrpcClient from '../../contexts/GrpcClient/GrpcClient';
 import * as Notifications from '../../../app/contexts/Notifications';
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import icon from './icon.svg';
 
 export type ButtonProps = {};
