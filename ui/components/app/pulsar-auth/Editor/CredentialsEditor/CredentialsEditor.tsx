d2ca8de49bfc2095cb7991cc511488120612783a
visortelle <visortelle@gmail.com>
2023-07-11 10:31:30 +0200

    Update CredentialsEditor.tsx

--- ui/components/app/pulsar-auth/Editor/CredentialsEditor/CredentialsEditor.tsx
+++ ui/components/app/pulsar-auth/Editor/CredentialsEditor/CredentialsEditor.tsx
@@ -19,8 +19,7 @@ const CredentialsEditor: React.FC<CredentialsEditorProps> = (props) => {
   const [credentialsName, setCredentialsName] = React.useState<string>('');
   const [credentials, setCredentials] = React.useState<Credentials>({ type: 'empty' });
   const { config } = AppContext.useContext();
-  const { notifyInfo, notifyError } = Notifications.useContext();
-  const [errors, setErrors] = React.useState<string[]>([]);
+  const { notifyError } = Notifications.useContext();
 
   return (
     <div className={s.CredentialsEditor}>
@@ -69,19 +68,11 @@ const CredentialsEditor: React.FC<CredentialsEditorProps> = (props) => {
         )}
       </div>
 
-      {errors.length > 0 && (
-        <div className={s.Errors}>
-          {errors.map((error, i) => (
-            <div key={i}>{error}</div>
-          ))}
-        </div>
-      )}
-
       <div className={s.Footer}>
         <Button type='regular' text='Cancel' onClick={props.onDone} />
         <Button
           type='primary'
-          disabled={credentialsName.length === 0 || errors.length > 0}
+          disabled={credentialsName.length === 0}
           text='Save'
           onClick={async () => {
             const res = await fetch(`${config.publicUrl}/pulsar-auth/add/${encodeURIComponent(credentialsName)}`, {
@@ -94,9 +85,9 @@ const CredentialsEditor: React.FC<CredentialsEditorProps> = (props) => {
             .then(async res => {
               if (res.status === 400) {
                 let errorBody = await res.text();
-                notifyInfo(errorBody);
+                notifyError(errorBody);
               } else if (res.status !== 200) {
-                notifyInfo('Server error happened.');
+                notifyError('Server error happened.');
               } else {
                 props.onDone();
               }
