6d0d184353297daaec37b0fa484a9e896fa4671e
visortelle <visortelle@gmail.com>
2023-07-14 13:38:16 +0200

    Debug build

--- docker/demoapp/get-tag.sh
+++ docker/demoapp/get-tag.sh
@@ -5,5 +5,5 @@ set -xeo pipefail
 this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 repo_dir=${this_dir}/../..
 
-app_version=$(cd "${repo_dir}/demoapp" && sbt version 2> /dev/null | tail -n 1 | cut -d ' ' -f 2)
+app_version=$(cd "${repo_dir}/demoapp" && sbt version 2> /dev/null | grep info | tail -n 1 | cut -d ' ' -f 2)
 echo "tealtools/pulsocat-demoapp:${app_version}"
--- docker/pulsocat/get-tag.sh
+++ docker/pulsocat/get-tag.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 repo_dir=${this_dir}/..
 
-app_version=$(cd "${repo_dir}/server" && sbt version 2> /dev/null | tail -n 1 | cut -d ' ' -f 2)
+app_version=$(cd "${repo_dir}/server" && sbt version 2> /dev/null | grep info | tail -n 1 | cut -d ' ' -f 2)
 echo "tealtools/pulsocat:${app_version}"
