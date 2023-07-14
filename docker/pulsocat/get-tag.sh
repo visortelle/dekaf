bf4a5c8b48197b163818ed51986f0e098a3d5515
visortelle <visortelle@gmail.com>
2023-07-14 11:08:30 +0200

    Debug build

--- docker/pulsocat/get-tag.sh
+++ docker/pulsocat/get-tag.sh
@@ -6,4 +6,4 @@ this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 repo_dir=${this_dir}/..
 
 app_version=$(cd "${repo_dir}/server" && sbt version 2> /dev/null | tail -n 1 | cut -d ' ' -f 2)
-echo "tesltools/pulsocat:${app_version}"
+echo "tealtools/pulsocat:${app_version}"
