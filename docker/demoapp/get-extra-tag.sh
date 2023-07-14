adf224827c9ae5064c6c547566c4308622ba2c2e
visortelle <visortelle@gmail.com>
2023-07-14 15:09:26 +0200

    Fix build

--- docker/demoapp/get-extra-tag.sh
+++ docker/demoapp/get-extra-tag.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 git_branch=$( git rev-parse --abbrev-ref HEAD )
-image_extra_tag=$("tealtools/pulsocat-demoapp:${git_branch}")
+image_extra_tag="tealtools/pulsocat-demoapp:${git_branch}"
 
 echo "${image_extra_tag}"
