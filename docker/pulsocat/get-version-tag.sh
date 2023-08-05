7c51c12d7e209a78372c16369f03621529ea9708
visortelle <visortelle@gmail.com>
2023-08-05 10:11:25 +0300

    Build docker image on CI

--- docker/pulsocat/get-version-tag.sh
+++ docker/pulsocat/get-version-tag.sh
@@ -7,6 +7,7 @@ repo_dir=${this_dir}/../..
 
 # If this is a public release, use the public repo.
 # Otherwise, use the dev repo.
+set +x
 git describe --tags
 is_public_release=$?
 if [ $is_public_release -eq 0 ]; then
@@ -14,6 +15,7 @@ if [ $is_public_release -eq 0 ]; then
 else
   docker_repo="pulsocat-dev"
 fi
+set -x
 
 app_version=$(cd "${repo_dir}/server" && sbt version 2> /dev/null | grep info | tail -n 1 | cut -d ' ' -f 2 | sed -e 's/\x1b\[[0-9;]*m//g' )
 echo "tealtools/${docker_repo}:${app_version}"
