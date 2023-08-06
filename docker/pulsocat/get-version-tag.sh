81b072e1bdd54dcd776ddc7dbb7fe60e79a490ec
visortelle <visortelle@gmail.com>
2023-08-06 12:31:42 +0300

    Fix build

--- docker/pulsocat/get-version-tag.sh
+++ docker/pulsocat/get-version-tag.sh
@@ -8,14 +8,15 @@ repo_dir=${this_dir}/../..
 # If this is a public release, use the public repo.
 # Otherwise, use the dev repo.
 set +e
-git describe --tags &> /dev/null
+git --no-pager tag --contains | grep +* &> /dev/null
 is_public_release=$?
-if [ $is_public_release -eq 0 ]; then
+if [ "$is_public_release" == "true" ]; then
   docker_repo="pulsocat"
 else
   docker_repo="pulsocat-dev"
 fi
 set -e
+
 
 app_version=$(cd "${repo_dir}/server" && sbt version 2> /dev/null | grep info | tail -n 1 | cut -d ' ' -f 2 | sed -e 's/\x1b\[[0-9;]*m//g' )
 echo "tealtools/${docker_repo}:${app_version}"
