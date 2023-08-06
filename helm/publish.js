a53240c6a1b7104343c4fa37570013dc9f8bf5a3
visortelle <visortelle@gmail.com>
2023-08-06 16:59:57 +0300

    Publish Helm Chart

--- helm/publish.js
+++ helm/publish.js
@@ -2,23 +2,63 @@
 
 const execSync = require('child_process').execSync;
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
 const thisDir = __dirname;
 const chartRoot = `${thisDir}/pulsocat-helm`;
-const chartYaml = `${chartRoot}/Chart.yaml`;
-const chartYamlContent = fs.readFileSync(chartYaml, 'utf8');
-const publicReleaseVersion = execSync('git --no-pager tag --contains', { encoding: 'utf-8' }).toString().trim();
 
-if (publicReleaseVersion) {
+const publicReleaseVersion = execSync('git --no-pager tag --contains', { encoding: 'utf-8' }).toString().trim();
+const chartName = publicReleaseVersion ? 'pulsocat-helm' : 'pulsocat-helm-dev';
+const chartVersion = publicReleaseVersion ? publicReleaseVersion.replace(/^v/, '') : `0.0.${new Date().getTime()}`;
+
+const tmpDir = path.join(os.tmpdir(), `pulsocat-helm-${new Date().getTime()}`);
+console.info(`Copying Helm Chart to ${tmpDir}`);
+execSync(`mkdir -p ${tmpDir}`);
+execSync(`cp -r "${chartRoot}" ${tmpDir}/`);
+const tmpChartRoot = `${tmpDir}/pulsocat-helm`;
+const chartYaml = `${tmpChartRoot}/Chart.yaml`;
+
+try {
+  console.info(`Setting ${chartName} as the name of Helm Chart.`);
+  const chartFiles = readDirRecursive(tmpDir);
+  console.log(chartFiles);
+  chartFiles.forEach(file => {
+    const fileContent = fs.readFileSync(file, 'utf8');
+    const newFileContent = fileContent.replace(/_chart-name/g, chartName);
+    fs.writeFileSync(file, newFileContent, { encoding: 'utf8' });
+  });
+
   console.info(`Setting ${publicReleaseVersion} as the version of Helm Chart.`);
-  const newChartYamlContent = chartYamlContent.replace(/^version: .*/gm, `version: ${publicReleaseVersion.replace(/^v/, '')}`);
+  const chartYamlContent = fs.readFileSync(chartYaml, 'utf8');
+  const newChartYamlContent = chartYamlContent.replace(/^version: .*/gm, `version: ${chartVersion}`);
   fs.writeFileSync(chartYaml, newChartYamlContent, { encoding: 'utf8' });
 
   console.info(`Pushing Helm Chart to the OCI registry...`);
   execSync(`echo $DOCKER_PASS | helm registry login --password-stdin --username $DOCKER_USER docker.io`);
-  execSync(`rm -rf ${thisDir}/*.tgz`);
-  execSync(`helm package ${chartRoot}`);
-  const packageTgz = execSync(`ls -1 ${thisDir}/*.tgz`).toString().trim().split('\n');
-  execSync(`helm push ${packageTgz} oci://docker.io/tealtools`);
-} else {
-  console.log('Not a public release. Skipping push Helm Chart.');
+  execSync(`rm -rf ${tmpDir}/*.tgz`);
+  execSync(`helm package ${tmpChartRoot}`, { cwd: tmpDir });
+  const packageTgz = execSync(`ls -1 ${tmpDir}/*.tgz`).toString().trim().split('\n');
+  execSync(`helm push ${packageTgz} oci://docker.io/tealtools`, { cwd: tmpDir });
+
+} catch (e) {
+  throw e;
+} finally {
+  console.info(`Cleaning up. Deleting ${tmpDir}`);
+  execSync(`rm -rf ${tmpDir}`);
+}
+
+// Thanks to Copilot :)
+function readDirRecursive(dir) {
+  const files = fs.readdirSync(dir);
+  const filesWithDir = files.map(file => `${dir}/${file}`);
+  const filesWithDirAndSubdir = filesWithDir.map(file => {
+    const stat = fs.statSync(file);
+    if (stat.isDirectory()) {
+      return readDirRecursive(file);
+    } else {
+      return file;
+    }
+  });
+  return filesWithDirAndSubdir.flat();
 }
--- helm/pulsocat-helm/templates/_helpers.tpl
+++ helm/pulsocat-helm/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "pulsocat-helm.name" -}}
+{{- define "_chart-name.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "pulsocat-helm.fullname" -}}
+{{- define "_chart-name.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "pulsocat-helm.chart" -}}
+{{- define "_chart-name.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "pulsocat-helm.labels" -}}
-helm.sh/chart: {{ include "pulsocat-helm.chart" . }}
-{{ include "pulsocat-helm.selectorLabels" . }}
+{{- define "_chart-name.labels" -}}
+helm.sh/chart: {{ include "_chart-name.chart" . }}
+{{ include "_chart-name.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,7 +45,7 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "pulsocat-helm.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "pulsocat-helm.name" . }}
+{{- define "_chart-name.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "_chart-name.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
