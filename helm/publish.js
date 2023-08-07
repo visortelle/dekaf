2718acf875d728fc8819f34ec8c9946b1051a092
visortelle <visortelle@gmail.com>
2023-08-07 13:33:27 +0300

    Upd

--- helm/publish.js
+++ helm/publish.js
@@ -8,44 +8,49 @@ const path = require('path');
 const thisDir = __dirname;
 const chartRoot = `${thisDir}/pulsocat-helm`;
 
-const publicReleaseVersion = execSync('git --no-pager tag --contains', { encoding: 'utf-8' }).toString().trim().replace(/^v/, '');
+function publishChart(name, version) {
+  const tmpDir = path.join(os.tmpdir(), `pulsocat-helm-${new Date().getTime()}`);
+  console.info(`Copying Helm Chart to ${tmpDir}`);
+  execSync(`mkdir -p ${tmpDir}`);
+  execSync(`cp -r "${chartRoot}" ${tmpDir}/`);
+  const tmpChartRoot = `${tmpDir}/pulsocat-helm`;
+  const chartYaml = `${tmpChartRoot}/Chart.yaml`;
+
+  try {
+    console.info(`Setting ${name} as the name of Helm Chart.`);
+    const chartFiles = readDirRecursive(tmpDir);
+    chartFiles.forEach(file => {
+      const fileContent = fs.readFileSync(file, 'utf8');
+      const newFileContent = fileContent.replace(/_chart-name/g, name);
+      fs.writeFileSync(file, newFileContent, { encoding: 'utf8' });
+    });
+
+    console.info(`Setting ${publicReleaseVersion} as the version of Helm Chart.`);
+    const chartYamlContent = fs.readFileSync(chartYaml, 'utf8');
+    const newChartYamlContent = chartYamlContent.replace(/^version: .*/gm, `version: ${chartVersion}`);
+    fs.writeFileSync(chartYaml, newChartYamlContent, { encoding: 'utf8' });
+
+    console.info(`Pushing Helm Chart to the OCI registry...`);
+    execSync(`echo $DOCKER_PASS | helm registry login --password-stdin --username $DOCKER_USER docker.io`);
+    execSync(`rm -rf ${tmpDir}/*.tgz`);
+    execSync(`helm package ${tmpChartRoot}`, { cwd: tmpDir });
+    const packageTgz = execSync(`ls -1 ${tmpDir}/*.tgz`).toString().trim().split('\n');
+    execSync(`helm push ${packageTgz} oci://docker.io/tealtools`, { cwd: tmpDir });
+  } catch (e) {
+    throw e;
+  } finally {
+    console.info(`Cleaning up. Deleting ${tmpDir}`);
+    execSync(`rm -rf ${tmpDir}`);
+  }
+}
+
 const gitRev = execSync('git rev-parse --short HEAD', { encoding: 'utf-8' }).toString().trim();
-const chartName = publicReleaseVersion ? 'pulsocat-helm' : 'pulsocat-helm-dev';
-const chartVersion = publicReleaseVersion ? publicReleaseVersion : `0.0.0-${gitRev}`;
+const publicReleaseVersion = execSync('git --no-pager tag --contains', { encoding: 'utf-8' }).toString().trim().replace(/^v/, '');
 
-const tmpDir = path.join(os.tmpdir(), `pulsocat-helm-${new Date().getTime()}`);
-console.info(`Copying Helm Chart to ${tmpDir}`);
-execSync(`mkdir -p ${tmpDir}`);
-execSync(`cp -r "${chartRoot}" ${tmpDir}/`);
-const tmpChartRoot = `${tmpDir}/pulsocat-helm`;
-const chartYaml = `${tmpChartRoot}/Chart.yaml`;
+publishChart('pulsocat-helm-dev', `0.0.0-${gitRev}`);
 
-try {
-  console.info(`Setting ${chartName} as the name of Helm Chart.`);
-  const chartFiles = readDirRecursive(tmpDir);
-  chartFiles.forEach(file => {
-    const fileContent = fs.readFileSync(file, 'utf8');
-    const newFileContent = fileContent.replace(/_chart-name/g, chartName);
-    fs.writeFileSync(file, newFileContent, { encoding: 'utf8' });
-  });
-
-  console.info(`Setting ${publicReleaseVersion} as the version of Helm Chart.`);
-  const chartYamlContent = fs.readFileSync(chartYaml, 'utf8');
-  const newChartYamlContent = chartYamlContent.replace(/^version: .*/gm, `version: ${chartVersion}`);
-  fs.writeFileSync(chartYaml, newChartYamlContent, { encoding: 'utf8' });
-
-  console.info(`Pushing Helm Chart to the OCI registry...`);
-  execSync(`echo $DOCKER_PASS | helm registry login --password-stdin --username $DOCKER_USER docker.io`);
-  execSync(`rm -rf ${tmpDir}/*.tgz`);
-  execSync(`helm package ${tmpChartRoot}`, { cwd: tmpDir });
-  const packageTgz = execSync(`ls -1 ${tmpDir}/*.tgz`).toString().trim().split('\n');
-  execSync(`helm push ${packageTgz} oci://docker.io/tealtools`, { cwd: tmpDir });
-
-} catch (e) {
-  throw e;
-} finally {
-  console.info(`Cleaning up. Deleting ${tmpDir}`);
-  execSync(`rm -rf ${tmpDir}`);
+if (publicReleaseVersion) {
+  publishChart('pulsocat-helm', publicReleaseVersion);
 }
 
 // Thanks to Copilot :)
