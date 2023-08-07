21d078aab326eb2ea29e261fe13eedef2e5c22ea
visortelle <visortelle@gmail.com>
2023-08-07 16:40:33 +0300

    Deploy pulsocat demo

--- deployment/pulsocat-demo/index.ts
+++ deployment/pulsocat-demo/index.ts
@@ -1,8 +1,3 @@
 import * as pulsocat from "./pulsocat/pulsocat";
 
-import { kubeLogin } from './kube-auth/get-kube-server-uri';
-
-kubeLogin();
-process.exit(1);
-
 const pulsocatResources = pulsocat.createResources();
