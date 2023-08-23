0895ed6f5305feeca761ae5c911ab91fee5fc1f8
visortelle <visortelle@gmail.com>
2023-08-23 12:34:41 +0300

    Adjust Helm chart configuration

--- deployment/pulsocat/pulsocat/pulsocat.ts
+++ deployment/pulsocat/pulsocat/pulsocat.ts
@@ -57,11 +57,11 @@ export const createResources = () => {
         },
 
         basePath: "/demo",
-        // config: {
-        //   publicBaseUrl: `https://${host}/demo`,
-        //   pulsarBrokerUrl: "pulsar+ssl://instance-f.o-xy6ek.snio.cloud:6651",
-        //   pulsarHttpUrl: "https://cluster-f.o-xy6ek.snio.cloud",
-        // },
+        config: {
+          publicBaseUrl: `https://${host}/demo`,
+          pulsarBrokerUrl: "pulsar+ssl://instance-f.o-xy6ek.snio.cloud:6651",
+          pulsarHttpUrl: "https://cluster-f.o-xy6ek.snio.cloud",
+        },
         env: [
           { name: "PULSOCAT_LICENSE_ID", value: "db1fa160-7f2f-4bdf-b3f2-5e194d2af2f6" },
           { name: "PULSOCAT_LICENSE_TOKEN", value: "activ-44d2d91a3f7a41a0ff35d3d7936ffd8ev3" },
