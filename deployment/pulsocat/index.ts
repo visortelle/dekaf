ec255a71c425bfebf7312976b1663a317d38f856
visortelle <visortelle@gmail.com>
2023-08-31 20:28:07 +0400

    upd

--- deployment/pulsocat/index.ts
+++ deployment/pulsocat/index.ts
@@ -1,6 +1,3 @@
 import * as pulsocat from "./pulsocat/pulsocat";
-import { mkIsPublicDemo } from "./shared/shared";
-
-const isPublicDemo = mkIsPublicDemo();
 
 const pulsocatResources = pulsocat.createResources();
