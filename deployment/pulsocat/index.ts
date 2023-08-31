abce97e353fccd239107fa5f3df284f823efb54f
visortelle <visortelle@gmail.com>
2023-08-31 19:15:19 +0400

    upd

--- deployment/pulsocat/index.ts
+++ deployment/pulsocat/index.ts
@@ -1,13 +1,6 @@
 import * as pulsocat from "./pulsocat/pulsocat";
-import * as grafana from "./grafana/grafana";
 import { mkIsPublicDemo } from "./shared/shared";
 
 const isPublicDemo = mkIsPublicDemo();
 
 const pulsocatResources = pulsocat.createResources();
-
-// if (isPublicDemo) {
-//   const grafanaResources = grafana.createResources({
-//     namespace: pulsocatResources.namespace,
-//   });
-// }
