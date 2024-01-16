96347a30473699f2365315adfbf06e83f1e28bdd
Kiryl Valkovich <kiryl_valkovich@teal.tools>
2024-01-16 17:47:03 +0400

    Desktop fix bugs (#269)
    - Fixed many bugs related local Pulsar instance lifecycle
    - Changed desktop app icon
    - Added "Edit License Info" dialog to the desktop app.
    
    <img width="974" alt="Screenshot 2024-01-16 at 4 44 58 PM"
    src="https://github.com/tealtools/pulsar-ui/assets/9302460/e775b246-a99b-4f15-babb-3f17976017d6">
    
    Former-commit-id: f179a4eb4743cdfe212a68c21c59449c219d52ca

--- desktop/electron/src/renderer/ui/LogsView/ProcessLogsView/ProcessLogsView.tsx
+++ desktop/electron/src/renderer/ui/LogsView/ProcessLogsView/ProcessLogsView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import s from './ProcessLogsView.module.css'
 import LogsView, { LogEntry } from '../LogsView';
 import { apiChannel, logsChannel } from '../../../../main/channels';
@@ -11,61 +11,14 @@ export type LogSource = {
 };
 
 export type ProcessLogsViewProps = {
-  sources: LogSource[]
+  logs: LogEntry[],
+  onClear: () => void
 };
 
-const maxEntries = 10_000;
-
 const ProcessLogsView: React.FC<ProcessLogsViewProps> = (props) => {
-  const [logs, setLogs] = useState<LogEntry[]>([]);
-
-  useEffect(() => {
-    const correlationId = uuid();
-
-    props.sources.forEach(s => {
-      const req: ResendProcessLogs = {
-        type: 'ResendProcessLogs',
-        correlationId,
-        processId: s.processId
-      };
-      window.electron.ipcRenderer.sendMessage(apiChannel, req);
-    });
-
-    window.electron.ipcRenderer.on(logsChannel, (arg) => {
-      if (arg.type === "ResendProcessLogsResult" && arg.correlationId === correlationId) {
-        setLogs(oldLogs => {
-          const newEntries: LogEntry[] = arg.entries.map(entry => {
-            const source = props.sources.find(s => s.processId === entry.processId)?.name || 'unknown';
-            return {
-              source,
-              content: entry.content,
-              epoch: entry.epoch
-            }
-          })
-          return oldLogs.concat(newEntries).sort((a, b) => a.epoch - b.epoch).slice(-maxEntries);
-        });
-      }
-
-      if (arg.type === "ProcessLogEntryReceived") {
-        const source = props.sources.find(s => s.processId === arg.entry.processId);
-
-        if (source === undefined) {
-          return;
-        }
-
-        const logEntry: LogEntry = {
-          source: source.name,
-          content: arg.entry.content,
-          epoch: arg.entry.epoch
-        }
-        setLogs(v => v.concat([logEntry]).slice(-maxEntries));
-      }
-    });
-  }, []);
-
   return (
     <div className={s.ProcessLogsView} style={{ overflow: 'hidden', display: 'flex' }}>
-      <LogsView logs={logs} />
+      <LogsView logs={props.logs} onClear={props.onClear} />
     </div>
   );
 }
