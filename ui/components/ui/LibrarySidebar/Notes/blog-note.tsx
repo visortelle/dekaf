3d83ecd5721a3bb5e7d49943c855c1a38063ffde
visortelle <visortelle@gmail.com>
2024-01-18 11:23:50 +0300

    Show product updates at the Instance page

---
+++ ui/components/ui/LibrarySidebar/Notes/blog-note.tsx
@@ -0,0 +1,13 @@
+import { ManagedMarkdownDocument } from "../../../ui/LibraryBrowser/model/user-managed-items";
+
+export const blogNote: ManagedMarkdownDocument = {
+  metadata: {
+    descriptionMarkdown: '',
+    id: '807e1935-e71d-4dbb-b21c-9cb24074b11f',
+    name: '⭐️ Updates',
+    type: 'markdown-document'
+  },
+  spec: {
+    markdown: ``
+  }
+};
