b86c81a45c181fe08afec9a8216784594fb26d22
Kiryl Valkovich <kiryl_valkovich@teal.tools>
2024-02-21 15:00:58 +0400

    One more bunch of fixes (#279)
    - Show the consumer sessions list in the library sidebar
    - Adjust topic page menu items
    - Improve tenant overview page layout
    - Many small fixes

---
+++ ui/components/ui/LibrarySidebar/Library/AllObjectTypes/AllObjectTypes.tsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import s from './AllObjectTypes.module.css'
+import { LibraryContext, resourceMatcherFromContext } from '../../../LibraryBrowser/model/library-context';
+import SaveLibraryItemButton from '../../../LibraryBrowser/LibraryBrowserPanel/LibraryBrowserButtons/SaveLibraryItemButton/SaveLibraryItemButton';
+import { ManagedItemType } from '../../../LibraryBrowser/model/user-managed-items';
+import { getReadableItemType } from '../../../LibraryBrowser/get-readable-item-type';
+import NoData from '../../../NoData/NoData';
+import FormLabel from '../../../ConfigurationTable/FormLabel/FormLabel';
+import { help } from '../../../LibraryBrowser/LibraryBrowserPanel/help';
+import BrowseLibraryButton from './BrowseLibraryItemButton/BrowseLibraryButton';
+import { ResourceMatcher } from '../../../LibraryBrowser/model/resource-matchers';
+import ResourceMatchersInput from '../../../LibraryBrowser/SearchEditor/ResourceMatchersInput/ResourceMatchersInput';
+import { H3 } from '../../../H/H';
+import { getDefaultLibraryItem } from '../../../LibraryBrowser/default-library-items';
+
+export type AllObjectTypesProps = {
+  libraryContext: LibraryContext,
+  onCount: (v: number) => void
+};
+
+const itemTypes: ManagedItemType[] = [
+  "consumer-session-config",
+  "consumer-session-target",
+  "topic-selector",
+  "message-filter",
+  "message-filter-chain",
+  "consumer-session-start-from",
+  "coloring-rule",
+  "coloring-rule-chain",
+  "markdown-document",
+  "basic-message-filter-target",
+  "value-projection",
+  "value-projection-list",
+  "deserializer"
+];
+
+const AllObjectTypes: React.FC<AllObjectTypesProps> = (props) => {
+  const [itemCountPerType, setItemCountPerType] = useState<Partial<Record<ManagedItemType, number>>>({});
+  const [resourceMatchers, setResourceMatchers] = useState<ResourceMatcher[]>([]);
+  const [refreshItemCountKey, setRefreshItemCountKey] = useState(0);
+
+  useEffect(() => {
+    setResourceMatchers([resourceMatcherFromContext(props.libraryContext, 'derive-from-context')]);
+  }, [props.libraryContext]);
+
+  useEffect(() => {
+    const count = Object.values(itemCountPerType)
+      .filter(it => it !== undefined)
+      .reduce((total, n) => total + n, 0);
+
+    props.onCount(count);
+  }, [itemCountPerType]);
+
+  return (
+    <div className={s.Library}>
+      <div className={s.ResourceMatchers}>
+        <H3>Search in Contexts</H3>
+
+        <ResourceMatchersInput
+          value={resourceMatchers}
+          onChange={setResourceMatchers}
+          libraryContext={props.libraryContext}
+        />
+      </div>
+
+      <div>
+        {
+          itemTypes.sort((a, b) => getReadableItemType(a).localeCompare(getReadableItemType(b))).map(itemType => {
+            const itemCount = itemCountPerType[itemType] ?? 0;
+
+            return (
+              <div key={itemType}>
+                <div className={s.ItemType}>
+                  <div className={s.ItemTypeHeader}>
+                    <div style={{ marginRight: '24rem' }}>
+                      <FormLabel
+                        content={getReadableItemType(itemType)}
+                        help={help[itemType]}
+                      />
+                    </div>
+
+                    {itemCount === 0 && (
+                      <div style={{ display: 'flex', justifySelf: 'center', marginLeft: '-12rem' }}>
+                        <NoData />
+                      </div>
+                    )}
+
+                    <BrowseLibraryButton
+                      key={refreshItemCountKey}
+                      itemType={itemType}
+                      libraryContext={props.libraryContext}
+                      availableForContexts={resourceMatchers}
+                      onItemCount={(itemCount) => {
+                        setItemCountPerType(v => ({
+                          ...v,
+                          [itemType]: itemCount || 0
+                        }));
+                      }}
+                      isHideSelectButton={itemType !== 'consumer-session-config'}
+                    />
+
+                    <SaveLibraryItemButton
+                      item={getDefaultLibraryItem(itemType, props.libraryContext).spec}
+                      libraryContext={props.libraryContext}
+                      onSaved={() => setRefreshItemCountKey(v => v + 1)}
+                      appearance='create'
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default AllObjectTypes;
--- ui/components/ui/LibrarySidebar/LibrarySidebar.tsx
+++ ui/components/ui/LibrarySidebar/LibrarySidebar.tsx
@@ -3,9 +3,9 @@ import s from './LibrarySidebar.module.css'
 import Tabs from '../Tabs/Tabs';
 import Notes from './Notes/Notes';
 import { LibraryContext } from '../LibraryBrowser/model/library-context';
-import Library from './Library/Library';
 import objectHash from 'object-hash';
 import NoData from '../NoData/NoData';
+import Library from './Library/Library';
 
 export type LibrarySidebarProps = {
   libraryContext: LibraryContext
@@ -22,7 +22,7 @@ type ItemsCount = {
 }
 
 const LibrarySidebar: React.FC<LibrarySidebarProps> = (props) => {
-  const [activeTab, setActiveTab] = useState<TabKey>('notes');
+  const [activeTab, setActiveTab] = useState<TabKey>('library');
   const [itemsCount, setItemsCount] = useState<ItemsCount>({
     notes: undefined,
     favorites: undefined,
@@ -36,25 +36,27 @@ const LibrarySidebar: React.FC<LibrarySidebarProps> = (props) => {
       <Tabs<TabKey>
         tabs={[
           {
-            key: 'notes',
-            title: <span style={{ display: 'inline-flex', gap: '1ch'}}>🗒 Notes{itemsCount.notes === undefined ? <NoData /> : <>&nbsp;<strong>{itemsCount.notes}</strong></>}</span>,
-            render: () => (
-              <Notes
-                key={reactKey}
-                libraryContext={props.libraryContext}
-                onCount={(v) => setItemsCount(ic => ({ ...ic, notes: v }))}
-              />
-            ),
-            isRenderAlways: true,
-          },
-          {
             key: 'library',
-            title: <span style={{ display: 'inline-flex', gap: '1ch'}}>📚 Library{itemsCount.library === undefined ? <NoData /> : <>&nbsp;<strong>{itemsCount.library}</strong></>}</span>,
+            title: <span style={{ display: 'inline-flex', gap: '1ch' }}>📚 Library{itemsCount.library === undefined ? <NoData /> : <>&nbsp;<strong>{itemsCount.library}</strong></>}</span>,
             render: () => (
               <Library
                 key={reactKey}
                 libraryContext={props.libraryContext}
                 onCount={(v) => setItemsCount(ic => ({ ...ic, library: v }))}
+              />
+            ),
+            isRenderAlways: true,
+            style: { overflow: 'hidden' }
+          },
+          {
+            key: 'notes',
+            title: <span style={{ display: 'inline-flex', gap: '1ch' }}>🗒 Notes{itemsCount.notes === undefined ? <NoData /> : <>&nbsp;<strong>{itemsCount.notes}</strong></>}</span>,
+            render: () => (
+              <Notes
+                key={reactKey}
+                libraryContext={props.libraryContext}
+                onCount={(v) => setItemsCount(ic => ({ ...ic, notes: v }))}
+                isVisible={activeTab === 'notes'}
               />
             ),
             isRenderAlways: true,
