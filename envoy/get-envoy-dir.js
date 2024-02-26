7236d280f81cf4a1edafbd68ab8f45d40bd7f0e6
visortelle <visortelle@gmail.com>
2024-02-26 11:48:11 +0400

    Debug windows build

--- envoy/get-envoy-dir.js
+++ envoy/get-envoy-dir.js
@@ -8,7 +8,7 @@ function getEnvoyBin() {
   if (platform === 'darwin' && arch === 'arm64') return "envoy/darwin/amd64/envoy.bin";
   if (platform === 'linux' && arch === 'x64') return "envoy/linux/amd64/envoy.bin";
   if (platform === 'linux' && arch === 'arm64') return "envoy/linux/arm64/envoy.bin";
-  if (platform === 'windows' && arch === 'x64') return "envoy/windows/amd64/envoy.exe";
+  if (platform === 'win32' && arch === 'x64') return "envoy/windows/amd64/envoy.exe";
   throw new Error(`Unsupported OS/architecture combination: ${arch}/${platform}`);
 }
 
