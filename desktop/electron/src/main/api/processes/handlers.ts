a6c75a38aa8d6e1ffeb2e8942f9e9bbc5c34c625
Alexey Samovich <47789499+alexesom@users.noreply.github.com>
2024-03-03 09:30:02 +0100

    Show size for local pulsar instances (#290)
    Former-commit-id: b9b1308313772c4adbf39d1575bc9af9e3835902 [formerly 467d917585179e7b1419337970d6e9ba1b642a28]
    Former-commit-id: 188a4cf9bd045fded33554c6ab74e6858fab4e07

--- desktop/electron/src/main/api/processes/handlers.ts
+++ desktop/electron/src/main/api/processes/handlers.ts
@@ -10,8 +10,8 @@ import fsAsync from 'fs/promises';
 import path from 'node:path';
 import { ErrorHappened } from "../api/types";
 import axios from 'axios';
-import { LocalPulsarInstance } from "../local-pulsar-instances/types";
-import { app, BrowserWindow, nativeImage } from "electron";
+import {LocalPulsarInstance} from "../local-pulsar-instances/types";
+import {app, BrowserWindow, nativeImage} from "electron";
 import portfinder from 'portfinder';
 import { colorsByName } from "../../../renderer/ui/ColorPickerButton/ColorPicker/color-palette";
 import { sendMessage } from "../api/send-message";
@@ -200,7 +200,7 @@ function updateActiveProcesses(newActiveProcesses: ActiveProcesses, newActiveChi
 export async function handleSpawnProcess(event: Electron.IpcMainEvent, arg: SpawnProcess): Promise<void> {
   try {
     if (arg.process.type === "pulsar-standalone") {
-      await runPulsarStandalone(arg.process.instanceId, event);
+      await runPulsarStandalone(arg.process.instanceId, arg.process.instanceConfig, event);
       return;
     } else if (arg.process.type === "dekaf") {
       await runDekaf(arg.process.connection, event, arg.process.isOpenInBrowser);
@@ -261,7 +261,7 @@ export async function handleResendProcessLogs(event: Electron.IpcMainEvent, arg:
   event.reply(logsChannel, req);
 }
 
-export async function runPulsarStandalone(instanceId: string, event: Electron.IpcMainEvent) {
+export async function runPulsarStandalone(instanceId: string, instanceConfig: LocalPulsarInstance, event: Electron.IpcMainEvent) {
   const paths = getPaths();
 
   const instancePaths = paths.getPulsarStandalonePaths(instanceId);
@@ -291,8 +291,6 @@ export async function runPulsarStandalone(instanceId: string, event: Electron.Ip
       throw new Error(`The local Pulsar instance is going to use port ${port}, that is already in use by another process or local Pulsar instance.`);
     }
   }
-
-  const instanceConfig = await getInstanceConfig(instanceId);
 
   const pulsarVersion = instanceConfig.config.pulsarVersion;
   const pulsarBin = paths.getPulsarBin(pulsarVersion);
