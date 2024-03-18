221253b26c13799ccf282f5d0e6bf70a8296979f
Alexey Samovich <47789499+alexesom@users.noreply.github.com>
2024-03-18 19:35:29 +0100

    Fix dekaf not starting in desktop app on Windows (#302)

--- server/src/main/scala/envoy/Envoy.scala
+++ server/src/main/scala/envoy/Envoy.scala
@@ -6,24 +6,32 @@ import org.apache.commons.lang3.SystemUtils
 import _root_.config.readConfig
 
 object Envoy:
-    def run: IO[Throwable, Unit] = for
-        envoyConfigProps <- readConfig.map(c =>
-            EnvoyConfigProps(
-              httpServerPort = c.internalHttpPort.get,
-              grpcServerPort = c.internalGrpcPort.get,
-              listenPort = c.port.get,
-              basePath = c.basePath.get,
+    def run: IO[Throwable, Unit] = 
+        val isDesktopBuild = buildinfo.ExtraBuildInfo.isDesktopBuild
+        
+        for
+            envoyConfigProps <- readConfig.map(c =>
+                EnvoyConfigProps(
+                httpServerPort = c.internalHttpPort.get,
+                grpcServerPort = c.internalGrpcPort.get,
+                listenPort = c.port.get,
+                basePath = c.basePath.get,
+                )
             )
-        )
 
-        envoyBinPath <- ZIO.succeed(if SystemUtils.IS_OS_WINDOWS then "envoy.exe" else "envoy.bin")
-        configPath <- getEnvoyConfigPath(envoyConfigProps).map(_.toString)
+            envoyBinPath <- ZIO.succeed(if SystemUtils.IS_OS_WINDOWS then "envoy.exe" else "envoy.bin")
+            configPath <- getEnvoyConfigPath(envoyConfigProps).map(_.toString)
 
-        _ <- ZIO.logInfo(s"Starting Envoy proxy with config: $configPath")
-        _ <- ZIO.logInfo(s"Listening port: ${envoyConfigProps.listenPort}")
+            _ <- ZIO.logInfo(s"Starting Envoy proxy with config: $configPath")
+            _ <- ZIO.logInfo(s"Listening port: ${envoyConfigProps.listenPort}")
 
-        process <- Command(envoyBinPath, "--config-path", configPath).run
+            process <- {
+                // For Windows Desktop app Dekaf doen't start without "inheritIO" for some reason
+                if isDesktopBuild then 
+                    Command(envoyBinPath, "--config-path", configPath).inheritIO.exitCode
+                else 
+                    Command(envoyBinPath, "--config-path", configPath).successfulExitCode
+            }
 
-        _ <- process.successfulExitCode
-        _ <- ZIO.never
-    yield ()
+            _ <- ZIO.never
+        yield ()
